3acea9ad93c5d0a93e7354680f8ee904feb68e0d
Nick Daugherty <ndaugherty987@gmail.com>
2015-11-30 12:31:32 -0700

    Add base bin file

---
+++ bin/vip.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+
+/**
+ * The command line vip tool
+ */
+
+process.title = 'vip';
+
+var program = require( 'commander' );
+
+program
+    .version( '0.0.1' )
+    .parse( process.argv );
+
+if ( ! process.argv.slice( 2 ).length ) {
+    program.outputHelp();
+}
