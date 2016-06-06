be153920366ba920987f212d59e7ae5d53a54b71
Josh Betz <j@joshbetz.com>
2016-06-06 20:27:40 +0000

    Add deploy sub-command.
    VIPers can deploy a given site.

--- bin/vip.js
+++ bin/vip.js
@@ -8,11 +8,35 @@ process.title = 'vip';
 
 var program = require( 'commander' );
 var package = require( '../package.json' );
+var api = require( '../src/api' );
+
+// TODO
+var is_vip = true;
 
 program
 	.version( package.version )
 	.command( 'configure', 'configure the cli settings' )
-	.parse( process.argv );
+
+// internal VIP commands
+if (!!is_vip) {
+	program
+		.command( 'deploy <site> <sha>' )
+		.description( 'deploy given git SHA')
+		.action( (site, sha) => {
+			// TODO: Make sha optional, deploy latest
+			// TODO: Take domain name for site
+
+			api
+				.post('/sites/' + site + '/revisions/' + sha + '/deploy' )
+				.end( err => {
+					if (err) {
+						console.error(err.response.error)
+					}
+				})
+		})
+}
+
+program.parse( process.argv );
 
 if ( ! process.argv.slice( 2 ).length ) {
 	program.outputHelp();
