4fd6c53a7d0b60d866d4bd52a2815d2f555f0063
Josh Betz <j@joshbetz.com>
2016-07-14 19:29:55 +0000

    Set the existing user ID as a default if it exists.
    Fixes #9

--- src/bin/vip-login.js
+++ src/bin/vip-login.js
@@ -10,24 +10,28 @@ var utils = require( '../src/utils' );
 program
 	.parse( process.argv );
 
-async.series({
-	userId: promptly.prompt.bind( null, 'User ID:' ),
-	accessToken: promptly.password.bind( null, 'Access Token:' )
-}, function( err, results ) {
-	if ( err ) {
-		return console.log( 'Error: ', err );
-	}
+utils.getCredentials( ( err, user ) => {
+	var userprompt = user && user.userId ? 'User ID (' + user.userId + '):' : 'User ID:';
 
-	var credentials = {
-		userId:      results.userId,
-		accessToken: results.accessToken
-	};
-
-	utils.setCredentials( credentials , function( err ) {
+	async.series({
+		userId: promptly.prompt.bind( null, userprompt, { default: user && user.userId ? user.userId : null } ),
+		accessToken: promptly.password.bind( null, 'Access Token:' )
+	}, function( err, results ) {
 		if ( err ) {
-			return console.log( 'Error setting credentials: ', err );
+			return console.log( 'Error: ', err );
 		}
 
-		return console.log( 'Configuration successful' );
+		var credentials = {
+			userId:      results.userId,
+			accessToken: results.accessToken
+		};
+
+		utils.setCredentials( credentials , function( err ) {
+			if ( err ) {
+				return console.log( 'Error setting credentials: ', err );
+			}
+
+			return console.log( 'Configuration successful' );
+		});
 	});
 });
