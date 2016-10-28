b902e0ea20c930f28e36001539fbcb9f6cf6e9c0
Josh Betz <j@joshbetz.com>
2016-10-28 18:25:07 +0000

    Detect invalid/expired tokens and ask to login again.
    Adds middleware to superagent functions which intercepts 401
    response codes and bails with an error message.
    
    Fixes #79

--- src/lib/api.js
+++ src/lib/api.js
@@ -13,4 +13,29 @@ utils.getCredentials( function( err, credentials ) {
 	api.auth.token = credentials.accessToken;
 });
 
-module.exports = api;
+function handleAuth( request ) {
+	var callback = request.callback;
+	request.callback = function( err, res ) {
+		if ( res.status === 401 ) {
+			return console.error( "Invalid or expired token. Please login with `vip login`" );
+		}
+
+		callback.call( request, err, res );
+	};
+}
+
+export function get( url ) {
+	return api.get(url).use(handleAuth);
+}
+
+export function post( url ) {
+	return api.post(url).use(handleAuth);
+}
+
+export function put( url ) {
+	return api.put(url).use(handleAuth);
+}
+
+export function del( url ) {
+	return api.del(url).use(handleAuth);
+}
