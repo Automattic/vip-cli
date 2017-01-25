b18223ae9568051eda57666c9ab967d1c62c1e39
Josh Betz <j@joshbetz.com>
2017-01-25 17:55:19 +0000

    Add log of skipped files
    See #105

--- src/bin/vip-import.js
+++ src/bin/vip-import.js
@@ -113,6 +113,7 @@ program
 
 					var access_token = res.body.data[0].meta_value;
 					var bar, filecount = 0;
+					var logfile = `/tmp/import-${site.client_site_id}-${Date.now()}.log`;
 
 					var processFiles = function( importing, callback ) {
 						var queue = async.priorityQueue( ( file, cb ) => {
@@ -161,11 +162,15 @@ program
 									ext = ext[ ext.length - 1 ];
 
 									if ( ! ext || ( options.types.indexOf( ext.toLowerCase() ) < 0 && options.extraTypes.indexOf( ext.toLowerCase() ) < 0 ) ) {
-										return cb( new Error( "Unsupported filetype: " + file ) );
+										return fs.appendFile( logfile + '-ext', file + '\n', function() {
+											return cb( new Error( "Unsupported filetype: " + file ) );
+										});
 									}
 
 									if ( ! options.intermediate && /-\d+x\d+\.\w{3,4}$/.test( file ) ) {
-										return cb( new Error( 'Skipping intermediate image: ' + file ) );
+										return fs.appendFile( logfile + '-int', file + '\n', function() {
+											return cb( new Error( 'Skipping intermediate image: ' + file ) );
+										});
 									}
 
 									if ( ! filepath[1] ) {
@@ -221,7 +226,37 @@ program
 					processFiles( false, function() {
 						bar = new progress( 'Importing [:bar] :percent (:current/:total) :etas', { total: filecount, incomplete: ' ', renderThrottle: 100 } );
 						console.log( 'Importing ' + filecount + ' files...' );
-						processFiles( true )
+						processFiles( true, function() {
+							let data;
+							let extHeader = "Skipped with unsupported extension:";
+							let intHeader = "Skipped intermediate images:";
+
+							extHeader += '\n' + '='.repeat(extHeader.length) + '\n\n';
+							intHeader += '\n' + '='.repeat(intHeader.length) + '\n\n';
+
+							fs.appendFileSync( logfile, extHeader );
+
+							try {
+								data = fs.readFileSync( logfile + '-ext' );
+								fs.appendFileSync( logfile, data + '\n\n' );
+								fs.unlinkSync( logfile + '-ext' );
+							} catch ( e ) {
+								fs.appendFileSync( logfile, "None\n\n" );
+							}
+
+
+							fs.appendFileSync( logfile, intHeader );
+
+							try {
+								data = fs.readFileSync( logfile + '-int' );
+								fs.appendFileSync( logfile, data + '\n\n' );
+								fs.unlinkSync( logfile + '-int' );
+							} catch ( e ) {
+								fs.appendFileSync( logfile, "None\n\n" );
+							}
+
+							console.log( `Import log: ${logfile}` );
+						});
 					});
 				});
 		});
