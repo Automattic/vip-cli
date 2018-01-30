45bfec1ec34d00967b5aabbd950182da447efbe0
Josh Betz <j@joshbetz.com>
2018-01-29 18:01:53 -0600

    Remove node-localstorage

--- src/lib/keychain.js
+++ src/lib/keychain.js
@@ -3,20 +3,41 @@ module.exports = {};
 if ( typeof window === 'undefined' || typeof window.localStorage === 'undefined' ) {
 	// node
 
+	const fs = require( 'fs' );
 	const os = require( 'os' );
-	const LocalStorage = require( 'node-localstorage' ).LocalStorage;
-	const localStorage = new LocalStorage( os.tmpdir() );
+
+	// read-only perms
+	const rw = 0o600;
+
+	let stat;
+	const file = os.tmpdir() + '/vip-go-cli';
+	try {
+		// Ensure the file exists
+		stat = fs.statSync( file );
+	} catch ( e ) {
+		const fd = fs.openSync( file, 'w+', rw );
+		fs.closeSync( fd );
+		stat = fs.statSync( file );
+	}
+
+	// Get file perms (last 3 bits of stat.mode)
+	const perms = stat.mode & 0o777;
+
+	// Ensure permissions are what we expect
+	if ( !! ( perms & ~rw ) ) {
+		throw 'Invalid permissions on access token file: ' + file;
+	}
 
 	module.exports.setPassword = function( service, password ) {
-		return localStorage.setItem( service, password );
+		return fs.writeFileSync( file, password );
 	};
 
 	module.exports.getPassword = function( service ) {
-		return localStorage.getItem( service );
+		return fs.readFileSync( file, 'utf8' );
 	};
 
 	module.exports.deletePassword = function( service ) {
-		return localStorage.removeItem( service );
+		return fs.unlinkSync( file );
 	};
 } else {
 	// browser
