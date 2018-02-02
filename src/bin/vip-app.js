a699ecdfa34a00d01f02e42a2e5e97f020b7dd74
Josh Betz <j@joshbetz.com>
2018-02-02 15:01:38 -0600

    Query apps by name in `vip app <app>`

--- src/bin/vip-app.js
+++ src/bin/vip-app.js
@@ -7,16 +7,36 @@ const API = require( '../lib/api' );
 args( { wildcardCommand: true, format: true } )
 	.command( 'list', 'List your VIP Go apps' )
 	.argv( process.argv, async ( arg, opts ) => {
+		const app = arg[ 0 ];
 		const api = await API();
-		const res = await api
-			.query( {
-				query: `{app(id:${ arg[ 0 ] }){
-					id,name,environments{id,name,defaultDomain,branch,datacenter}
-				}}`
-			} )
-			.catch( err => console.log( err ) );
 
-		if ( res ) {
+		if ( isNaN( parseInt( app ) ) ) {
+			const res = await api
+				.query( {
+					query: `{apps(name:"${ app }"){
+						id,name,environments{id,name,defaultDomain,branch,datacenter}
+					}}`
+				} )
+				.catch( err => { /* noop */ } );
+
+			if ( ! res || ! res.data || ! res.data.apps || ! res.data.apps.length ) {
+				return console.log( `App ${ app.blue } does not exist` );
+			}
+
+			return res.data.apps[ 0 ].environments;
+		} else {
+			const res = await api
+				.query( {
+					query: `{app(id:${ app }){
+						id,name,environments{id,name,defaultDomain,branch,datacenter}
+					}}`
+				} )
+				.catch( err => { /* noop */ } );
+
+			if ( ! res || ! res.data || ! res.data.app ) {
+				return console.log( `App ${ app.toString().blue } does not exist` );
+			}
+
 			return res.data.app.environments;
 		}
 	} );
