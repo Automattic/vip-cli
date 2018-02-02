3e3f37260012f6287a6a7db0b444a6e7bb443130
Josh Betz <j@joshbetz.com>
2018-02-02 21:57:43 +0000

    Don't set authorization header if token doesn't exist

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,9 +9,11 @@ const API_URL = process.env.API_URL || 'https://api.go-vip.co/graphql';
 
 module.exports = async function API() {
 	const token = await Token.get();
-	const headers = {
-		Authorization: `Bearer ${ token.raw }`,
-	};
+	const headers = {};
+
+	if ( token ) {
+		token.Authorization = `Bearer ${ token.raw }`;
+	}
 
 	return new FetchQL( { url: API_URL, headers: headers } );
 };
