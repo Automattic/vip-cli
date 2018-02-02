78728103d4bd8a89d6f072f98b4149222d24e5d4
Josh Betz <j@joshbetz.com>
2018-02-02 17:11:42 -0600

    Optionally include keytar
    Keytar doesn't work everywhere, but we can try installing it. If that
    doesn't work, we'll fall back to the current method.

--- src/lib/keychain.js
+++ src/lib/keychain.js
@@ -3,42 +3,62 @@ module.exports = {};
 if ( typeof window === 'undefined' || typeof window.localStorage === 'undefined' ) {
 	// node
 
-	const fs = require( 'fs' );
-	const os = require( 'os' );
-
-	// read-only perms
-	const rw = 0o600;
-
-	let stat;
-	const file = os.tmpdir() + '/vip-go-cli';
 	try {
-		// Ensure the file exists
-		stat = fs.statSync( file );
-	} catch ( e ) {
-		const fd = fs.openSync( file, 'w+', rw );
-		fs.closeSync( fd );
-		stat = fs.statSync( file );
+		// secure
+
+		const keytar = require( 'keytar' );
+
+		module.exports.setPassword = function( service, password ) {
+			return keytar.setPassword( service, service, password );
+		};
+
+		module.exports.getPassword = function( service ) {
+			return keytar.getPassword( service, service );
+		};
+
+		module.exports.deletePassword = function( service ) {
+			return keytar.deletePassword( service, service );
+		};
+	} catch ( _ ) {
+		// insecure fallback
+
+		const fs = require( 'fs' );
+		const os = require( 'os' );
+
+		// read-only perms
+		const rw = 0o600;
+
+		let stat;
+		const file = os.tmpdir() + '/vip-go-cli';
+		try {
+			// Ensure the file exists
+			stat = fs.statSync( file );
+		} catch ( __ ) {
+			const fd = fs.openSync( file, 'w+', rw );
+			fs.closeSync( fd );
+			stat = fs.statSync( file );
+		}
+
+		// Get file perms (last 3 bits of stat.mode)
+		const perms = stat.mode & 0o777;
+
+		// Ensure permissions are what we expect
+		if ( !! ( perms & ~rw ) ) {
+			throw 'Invalid permissions on access token file: ' + file;
+		}
+
+		module.exports.setPassword = function( service, password ) {
+			return fs.writeFileSync( file, password );
+		};
+
+		module.exports.getPassword = function( service ) {
+			return fs.readFileSync( file, 'utf8' );
+		};
+
+		module.exports.deletePassword = function( service ) {
+			return fs.unlinkSync( file );
+		};
 	}
-
-	// Get file perms (last 3 bits of stat.mode)
-	const perms = stat.mode & 0o777;
-
-	// Ensure permissions are what we expect
-	if ( !! ( perms & ~rw ) ) {
-		throw 'Invalid permissions on access token file: ' + file;
-	}
-
-	module.exports.setPassword = function( service, password ) {
-		return fs.writeFileSync( file, password );
-	};
-
-	module.exports.getPassword = function( service ) {
-		return fs.readFileSync( file, 'utf8' );
-	};
-
-	module.exports.deletePassword = function( service ) {
-		return fs.unlinkSync( file );
-	};
 } else {
 	// browser
 
