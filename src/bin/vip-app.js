2e3e16a411c170b803a87f216b7c6a4ef4111a7e
Josh Betz <j@joshbetz.com>
2018-02-07 19:25:18 -0600

    Use helper function to query app in `vip app <app>`

--- src/bin/vip-app.js
+++ src/bin/vip-app.js
@@ -1,44 +1,22 @@
 #!/usr/bin/env node
 // @flow
 
+const colors = require( 'colors' );
+
 // ours
 const args = require( '../lib/cli/command' );
-const API = require( '../lib/api' );
+const app = require( '../lib/api/app' );
 
 args( { requiredArgs: 1, format: true } )
 	.example( 'vip app <app>', 'Pass an app name or ID to get details about that app' )
 	.command( 'list', 'List your VIP Go apps' )
 	.argv( process.argv, async ( arg, opts ) => {
-		const app = arg[ 0 ];
-		const api = await API();
+		const res = await app( arg[ 0 ] );
 
-		if ( isNaN( parseInt( app ) ) ) {
-			const res = await api
-				.query( {
-					query: `{apps(name:"${ app }"){
-						id,name,environments{id,name,defaultDomain,branch,datacenter}
-					}}`
-				} )
-				.catch( err => { /* noop */ } );
-
-			if ( ! res || ! res.data || ! res.data.apps || ! res.data.apps.length ) {
-				return console.log( `App ${ app.blue } does not exist` );
-			}
-
-			return res.data.apps[ 0 ].environments;
+		if ( ! res || ! res.environments ) {
+			console.log( `App ${ colors.blue( arg[ 0 ] ) } does not exist` );
+			return {};
 		}
 
-		const res = await api
-			.query( {
-				query: `{app(id:${ app }){
-					id,name,environments{id,name,defaultDomain,branch,datacenter}
-				}}`
-			} )
-			.catch( err => { /* noop */ } );
-
-		if ( ! res || ! res.data || ! res.data.app ) {
-			return console.log( `App ${ app.toString().blue } does not exist` );
-		}
-
-		return res.data.app.environments;
+		return res.environments;
 	} );
