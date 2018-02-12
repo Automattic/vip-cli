7948b268f7b77fb91b2ab0b57e7cded8bd8cd293
Nick Daugherty <ndaugherty987@gmail.com>
2018-02-12 11:56:40 -0700

    Use relative files in the index.js includes
    Otherwise you get
    
    ```
    ./node_modules/@automattic/vip/index.js
    Module not found: Can't resolve 'dist/lib/api' in 'node_modules/@automattic/vip'```

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const api = require( 'dist/lib/api' );
-const token = require( 'dist/lib/token' );
+const api = require( './dist/lib/api' );
+const token = require( './dist/lib/token' );
 
 module.exports = {
 	api,
