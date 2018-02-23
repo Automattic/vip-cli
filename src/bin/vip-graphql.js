e36a0151a0c5a0fdeb2da6f608a35271543e46db
Josh Betz <j@joshbetz.com>
2018-02-23 12:15:12 -0600

    Add graphql command

---
+++ src/bin/vip-graphql.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+// @flow
+
+const gql = require( 'graphql-tag' );
+
+// ours
+const command = require( '../lib/cli/command' );
+const API = require( '../lib/api' );
+
+command( { requiredArgs: 1, format: true } )
+	.argv( process.argv, async ( arg, opts ) => {
+		const api = await API();
+
+		let res;
+		try {
+			res = await api
+				// $FlowFixMe
+				.query( { query: gql`query Query ${ arg[ 0 ] }` } );
+		} catch ( err ) {
+			console.log( err.toString() );
+			return;
+		}
+
+		const keys = Object.keys( res.data );
+		if ( keys.length < 1 ) {
+			console.log( 'Empty set' );
+			return;
+		}
+
+		return res.data[ keys[ 0 ] ];
+	} );
