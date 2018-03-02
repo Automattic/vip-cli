2f65088c1684982d1ca3425d6ca98a6d31911f60
Josh Betz <j@joshbetz.com>
2018-03-02 11:37:16 -0500

    Set vip app <app> query

--- src/bin/vip-app.js
+++ src/bin/vip-app.js
@@ -16,7 +16,7 @@ command( { requiredArgs: 1, format: true } )
 	.example( 'vip app <app>', 'Pass an app name or ID to get details about that app' )
 	.command( 'list', 'List your VIP Go apps' )
 	.argv( process.argv, async ( arg, opts ) => {
-		const res = await app( arg[ 0 ] );
+		const res = await app( arg[ 0 ], 'id,name,environments{id,name,defaultDomain}' );
 
 		if ( ! res || ! res.environments ) {
 			console.log( `App ${ colors.blue( arg[ 0 ] ) } does not exist` );
