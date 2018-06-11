b80479d0ce6bc024d7fc088c4458a66846c6ffba
Mohammad Jangda <batmoo@gmail.com>
2018-06-11 15:09:29 -0400

    Fix stringify of nested arrays
    The Node lib doesn't handle it very well, so we need to do it ourselves.

--- src/lib/analytics/tracks.js
+++ src/lib/analytics/tracks.js
@@ -34,11 +34,31 @@ export default class Tracks {
 			_en: name,
 		}, details );
 
-		const params = {
-			events: [
-				event,
-			],
-		};
+		// For when we want to support batched events
+		const events = [ event ];
+
+		/**
+		 * The API expects an indexed events array with event details.
+		 *
+		 * `querystring.stringify` does not handle nested arrays and objects very well.
+		 *
+		 * So we can just do it ourselves instead.
+		 *
+		 * Should end up with something like:
+		 *  - events[0][_en]=clickButton // event name
+		 *  - events[0][buttonName]=Deploy // event custom prop
+		 *  - events[1][_en]=loadPage
+		 */
+		const params = events.reduce( ( reduced, ev, index ) => {
+			Object.keys( ev ).forEach( key => {
+				const param = `events[${ index }][${ key }]`;
+				const value = event[ key ];
+
+				reduced[ param ] = value;
+			} );
+
+			return reduced;
+		}, {} );
 
 		return this.send( params );
 	}
