e9ddc0cd27c8959b2abb3d35fe87f24e9cfa6a2a
Mohammad Jangda <batmoo@gmail.com>
2018-06-12 22:46:34 -0700

    Add more detail to permissions error
    Include the current file permissions.

--- src/lib/keychain/insecure.js
+++ src/lib/keychain/insecure.js
@@ -35,7 +35,7 @@ export default class Insecure implements Keychain {
 
 		// Ensure permissions are what we expect
 		if ( !! ( perms & ~rw ) ) {
-			throw 'Invalid permissions on access token file: ' + tmpfile;
+			throw `Invalid permissions (${ perms }) for keychain file (${ tmpfile })`;
 		}
 
 		this.file = tmpfile;
