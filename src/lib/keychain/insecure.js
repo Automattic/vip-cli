d06c1348646e70619154e49df515b2dc03b8189a
Josh Betz <j@joshbetz.com>
2018-06-18 16:42:41 -0500

    Print permissions as base 8 in error messages

--- src/lib/keychain/insecure.js
+++ src/lib/keychain/insecure.js
@@ -39,7 +39,7 @@ export default class Insecure implements Keychain {
 
 		// Ensure permissions are what we expect
 		if ( !! ( perms & ~rw ) ) {
-			throw `Invalid permissions (${ perms }, expecting ${ rw }) for keychain file (${ tmpfile })`;
+			throw `Invalid permissions (${ perms.toString( 8 ) }, expecting ${ rw.toString( 8 ) }) for keychain file (${ tmpfile })`;
 		}
 
 		this.file = tmpfile;
