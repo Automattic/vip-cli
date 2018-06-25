d368e1c7c939bc723618c09338c1cec9526b5d4d
Josh Betz <j@joshbetz.com>
2018-06-25 17:04:45 -0500

    Use more readable IO names for child_process.stdio

--- src/lib/cli/pager.js
+++ src/lib/cli/pager.js
@@ -10,7 +10,7 @@ export default function pager() {
 	const args = ( process.env.PAGER || 'less -FRX' ).split( ' ' );
 	const bin = args.shift();
 
-	proc = spawn( bin, args, { stdio: [ -1, 1, 2 ] } );
+	proc = spawn( bin, args, { stdio: [ 'pipe', process.stdout, process.stderr ] } );
 	proc.on( 'exit', () => {
 		proc.stdin.emit( 'done' );
 	} );
