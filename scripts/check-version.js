bf18cb3296f5c68c20b3829178d58a5db1e33186
Gabriel Sambarino <gabriel.sambarino@gmail.com>
2018-07-10 16:38:56 +0200

    Bump up expected node version to 7.0
    This is needed for Object.values() to work. Anyway 7.0 is 2 years old so should not be a big deal.
    Version check added, too.

---
+++ scripts/check-version.js
@@ -0,0 +1,9 @@
+const semver = require( 'semver' );
+const { name, engines } = require( '../package.json' );
+
+const version = engines.node;
+
+if ( ! semver.satisfies( process.version, version ) ) {
+	console.log( `The current version of Node (${ process.version }) does not meet the minimum requirements; ${ name } requires Node version ${ version }. Please follow the installation instructions at https://nodejs.org/en/download/ to upgrade before continuing.` );
+	process.exit( 1 );
+}
