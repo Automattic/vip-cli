2d7e3377ed267c153e2094b162ace648bde1c823
Nick Daugherty <ndaugherty987@gmail.com>
2018-10-17 13:05:58 -0600

    Eslint autofixes

--- src/lib/analytics/index.js
+++ src/lib/analytics/index.js
@@ -5,14 +5,14 @@ import AnalyticsClientStub from './clients/stub';
 
 export default class Analytics {
 	constructor( {
-		tracks = new AnalyticsClientStub()
+		tracks = new AnalyticsClientStub(),
 	} ) {
 		this.tracks = tracks;
 	}
 
 	async trackEvent( name, props = {} ): Promise {
 		return await Promise.all( [
-			this.tracks.trackEvent( name, props )
+			this.tracks.trackEvent( name, props ),
 		] );
 	}
 }
