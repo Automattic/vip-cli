8b2be3c482ade018d19f5db89832e7721020b85b
Ahmed <elazzabi.ahmed@gmail.com>
2018-10-26 09:32:30 +0100

    add os, cli, and node version

--- src/lib/analytics/index.js
+++ src/lib/analytics/index.js
@@ -2,6 +2,13 @@
  * Internal dependencies
  */
 import AnalyticsClientStub from './clients/stub';
+import { version } from '../../../package.json';
+
+const client_info = {
+	node_version: process.version,
+	os: process.platform,
+	cli_version: version,
+}
 
 export default class Analytics {
 	constructor( {
@@ -12,7 +19,7 @@ export default class Analytics {
 
 	async trackEvent( name, props = {} ): Promise {
 		return await Promise.all( [
-			this.tracks.trackEvent( name, props ),
+			this.tracks.trackEvent( name, Object.assign( {}, client_info, props ) ),
 		] );
 	}
 }
