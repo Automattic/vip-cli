1b26fa13537cae3ff92da7039387fd86f66eb3e5
Nick Daugherty <ndaugherty987@gmail.com>
2019-03-01 12:07:37 -0700

    Misc CLI command tweaks - WIP

--- src/bin/vip-wp.js
+++ src/bin/vip-wp.js
@@ -22,12 +22,39 @@ import { trackEvent } from 'lib/tracker';
 const socket = SocketIO( 'http://localhost:4000/wp-cli' );
 
 command( {
-	requiredArgs: 3,
+	requiredArgs: 2,
 } )
 	.argv( process.argv, async ( arg, opts ) => {
 		const stdoutStream = IOStream.createStream();
+		const stdinStream = IOStream.createStream();
 
-		IOStream( socket ).emit( 'readStdout', stdoutStream );
+		// TODO handle all arguments
+		// TODO trigger mutation
+		// TODO subscribe to stream
+		// TODO handle disconnect - does IOStream correctly buffer stdin?
+		// TODO stderr - currently server doesn't support it, so errors don't terminate process
+
+		const cmd = arg.join( ' ' );
+
+		const data = {
+			cmd,
+			guid: 'some-generated-guid-from-mutation',
+			columns: process.stdout.columns,
+			rows: process.stdout.rows,
+		};
+
+		IOStream( socket ).emit( 'cmd', data, stdinStream, stdoutStream );
 
 		stdoutStream.pipe( process.stdout );
+
+		stdoutStream.on( 'error', err => {
+			// TODO handle this better
+			console.log( err );
+
+			process.exit( 1 );
+		} );
+
+		stdoutStream.on( 'end', () => {
+			process.exit();
+		} );
 	} );
