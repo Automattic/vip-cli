4a6e4244e71b52362f23c540851a3e12033afacd
Ahmed <elazzabi.ahmed@gmail.com>
2019-03-12 16:02:20 +0100

    add unauthorizedLink

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,6 +7,8 @@ require( 'isomorphic-fetch' );
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import chalk from 'chalk';
 
 /**
  * Internal dependencies
@@ -24,6 +26,13 @@ export default async function API(): Promise<ApolloClient> {
 	if ( token ) {
 		headers.Authorization = `Bearer ${ token.raw }`;
 	}
+
+	const unauthorizedLink = onError( ( { networkError } ) => {
+		if ( networkError.statusCode === 401 ) {
+			console.error( chalk.red( 'Error:' ), 'You are unauthorized to perform this request' );
+			process.exit();
+		};
+	})
 
 	return new ApolloClient( {
 		link: new HttpLink( { uri: API_URL, headers: headers } ),
