348168d3019a0fcfafc481faf564ea050e09b151
Nick Daugherty <ndaugherty987@gmail.com>
2019-03-12 12:14:21 -0600

    s/test/it

--- __tests__/lib/token.js
+++ __tests__/lib/token.js
@@ -5,7 +5,7 @@
 import Token from 'lib/token';
 
 describe( 'token tests', () => {
-	test( 'should correctly validate token', () => {
+	it( 'should correctly validate token', () => {
 		// Does not expire
 		const t = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWQiOjcsImlhdCI6MTUxNjIzOTAyMn0.RTJMXHhhiaCxQberZ5Pre7SBU3Ci8EvCyaOXoqG3pNA';
 		const token = new Token( t );
@@ -13,13 +13,13 @@ describe( 'token tests', () => {
 		expect( token.expired() ).toEqual( false );
 	} );
 
-	test( 'should correctly validate token missing an id', () => {
+	it( 'should correctly validate token missing an id', () => {
 		const t = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1MTYxMzUyNzYsImV4cCI6MjUyNDYwODAwMCwiYXVkIjoiIiwic3ViIjoiIn0.seD8rBKJS0usjYApigqizitlNcmzcrYlGt9DyCm3I4c';
 		const token = new Token( t );
 		expect( token.valid() ).toEqual( false );
 	} );
 
-	test( 'should error for invalid token', () => {
+	it( 'should error for invalid token', () => {
 		const t = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjRhM2RmYjE5LTBhMWQtNDE3YS05ODM2LTdjZWIwZTBkM2Q4NSIsImlhdCI6MTUxNjEyMzU1NywiZXhwIjoxNTE2MTI3zM4fQ.atx1YhxB6SQoW99aL97tXNlyJlXWEPZ3Cf1zyfxizvs';
 		let token;
 		try {
@@ -30,14 +30,14 @@ describe( 'token tests', () => {
 		expect( token ).toEqual( undefined );
 	} );
 
-	test( 'should not validate expired token', () => {
+	it( 'should not validate expired token', () => {
 		const t = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTE1NzExMDY5LCJleHAiOjE1MTU3OTc0Njl9.hZ-mAeoFAahak9WXqAVTOKEU7R_f1VsZfS5HqZOm-a4';
 		const token = new Token( t );
 		expect( token.valid() ).toEqual( false );
 		expect( token.expired() ).toEqual( true );
 	} );
 
-	test( 'should correctly validate token with invalid whitespace', () => {
+	it( 'should correctly validate token with invalid whitespace', () => {
 		const leadingWhitespace = ' eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwiaWQiOjcsImlhdCI6MTUxNjIzOTAyMn0.RTJMXHhhiaCxQberZ5Pre7SBU3Ci8EvCyaOXoqG3pNA';
 		let token = new Token( leadingWhitespace );
 		expect( token.valid() ).toEqual( true );
@@ -51,7 +51,7 @@ describe( 'token tests', () => {
 		expect( token.valid() ).toEqual( false );
 	} );
 
-	test( 'should consistently return uuid', () => {
+	it( 'should consistently return uuid', () => {
 		Token.uuid().then( uuid1 => {
 			Token.uuid().then( uuid2 => {
 				expect( uuid1 ).toBe( uuid2 );
