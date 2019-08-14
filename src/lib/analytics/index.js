399e8e9d98c570c1c80a6df174da7ae152af6dca
Ahmed <elazzabi.ahmed@gmail.com>
2019-08-14 12:33:40 +0100

    track is_vip flag

--- src/lib/analytics/index.js
+++ src/lib/analytics/index.js
@@ -3,6 +3,7 @@
  */
 import AnalyticsClientStub from './clients/stub';
 import env from '../env';
+import currentUser from 'lib/api/user';
 
 const client_info = {
 	cli_version: env.app.version,
@@ -19,8 +20,10 @@ export default class Analytics {
 	}
 
 	async trackEvent( name, props = {} ): Promise {
+		const user = await currentUser();
+
 		return await Promise.all( [
-			this.tracks.trackEvent( name, Object.assign( {}, client_info, props ) ),
+			this.tracks.trackEvent( name, Object.assign( { is_vip: user.me && user.me.isVIP }, client_info, props ) ),
 		] );
 	}
 }
