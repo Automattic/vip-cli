44c67b56865a853ae11c47e630273d857059aa36
Daryl Chymko <dchymko@infometrix.ca>
2020-06-24 23:35:27 -0700

    Simply output checks to encapusulate in check object

--- src/bin/vip-import-check.js
+++ src/bin/vip-import-check.js
@@ -17,6 +17,104 @@ import path from 'path';
  */
 import command from 'lib/cli/command';
 
+let problemsFound = 0;
+let lineNum = 1;
+
+const errorHandler = ( check ) => {
+	if ( check.results.length > 0 ) {
+		problemsFound += 1;
+		console.error( chalk.red( 'Error:' ), `${ check.message } on line(s) ${ check.results.join( ',' ) }.` );
+		console.error( chalk.yellow( 'Recommendation:' ), `${ check.recommendation }` );
+	} else {
+		console.log( `✅ ${ check.message } was found ${ check.results.length } times.` );
+	}
+};
+
+const requiredHandler = ( check, type ) => {
+	if ( check.results.length > 0 ) {
+		console.log( `✅ ${ check.message } was found ${ check.results.length } times.` );
+		if ( type === 'createTable' ) {
+			checkTables( check.results );
+		}
+	} else {
+		problemsFound += 1;
+		console.error( chalk.red( 'Error:' ), `${ check.message } was not found.` );
+		console.error( chalk.yellow( 'Recommendation:' ), `${ check.recommendation }` );
+	}
+};
+
+const infoHandler = ( check ) => {
+	check.results.forEach( item => {
+		console.log( item );
+	} );
+};
+
+const checks = {
+	useDB: {
+		matcher: /^use\s/i,
+		matchHandler: ( lineNumber ) => lineNumber,
+		outputHandler: errorHandler,
+		results: [],
+		message: 'USE statement',
+		excerpt: '\'USE\' statement should not be present (case-insensitive, at beginning of line)',
+		recommendation: 'Remove these lines',
+	},
+	createDB: {
+		matcher: /^CREATE DATABASE/i,
+		matchHandler: ( lineNumber ) => lineNumber,
+		outputHandler: errorHandler,
+		results: [],
+		message: 'CREATE DATABASE statement',
+		excerpt: '\'CREATE DATABASE\' statement should not  be present (case-insensitive)',
+		recommendation: 'Remove these lines',
+	},
+	dropDB: {
+		matcher: /^DROP DATABASE/i,
+		matchHandler: ( lineNumber ) => lineNumber,
+		outputHandler: errorHandler,
+		results: [],
+		message: 'DROP DATABASE statement',
+		excerpt: '\'DROP DATABASE\' should not be present (case-insensitive)',
+		recommendation: 'Remove these lines',
+	},
+	alterUser: {
+		matcher: /^(ALTER USER|SET PASSWORD)/i,
+		matchHandler: ( lineNumber ) => lineNumber,
+		outputHandler: errorHandler,
+		results: [],
+		message: 'ALTER USER statement',
+		excerpt: '\'ALTER USER\' should not be present (case-insensitive)',
+		recommendation: 'Remove these lines',
+	},
+	dropTable: {
+		matcher: /^DROP TABLE IF EXISTS `?([a-z0-9_]*)/i,
+		matchHandler: ( lineNumber, results ) => results [ 1 ],
+		outputHandler: requiredHandler,
+		results: [],
+		message: 'DROP TABLE',
+		excerpt: '\'DROP TABLE IF EXISTS\' should be present (case-insensitive)',
+		recommendation: 'Check import settings to include DROP TABLE statements',
+	},
+	createTable: {
+		matcher: /^CREATE TABLE `?([a-z0-9_]*)/i,
+		matchHandler: ( lineNumber, results ) => results [ 1 ],
+		outputHandler: requiredHandler,
+		results: [],
+		message: 'CREATE TABLE',
+		excerpt: '\'CREATE TABLE\' should be present (case-insensitive)',
+		recommendation: 'Check import settings to include CREATE TABLE statements',
+	},
+	siteHomeUrl: {
+		matcher: '\'(siteurl|home)\',\\s?\'(.*?)\'',
+		matchHandler: ( lineNumber, results ) => results [ 0 ],
+		outputHandler: infoHandler,
+		results: [],
+		message: 'Siteurl/home matches',
+		excerpt: 'Siteurl/home options',
+		recommendation: '',
+	},
+};
+
 command( {
 	requiredArgs: 1,
 } )
@@ -27,80 +125,11 @@ command( {
 			process.exit( 1 );
 		}
 
-		let problemsFound = 0;
-
 		const readInterface = readline.createInterface( {
 			input: fs.createReadStream( arg[ 0 ] ),
 			output: null,
 			console: false,
 		} );
-
-		const checks = {
-			useDB: {
-				type: 'error',
-				matcher: /^use\s/i,
-				matchHandler: ( lineNum ) => lineNum,
-				results: [],
-				message: 'USE statement',
-				excerpt: '\'USE\' statement should not be present (case-insensitive, at beginning of line)',
-				recommendation: 'Remove these lines',
-			},
-			createDB: {
-				type: 'error',
-				matcher: /^CREATE DATABASE/i,
-				matchHandler: ( lineNum ) => lineNum,
-				results: [],
-				message: 'CREATE DATABASE statement',
-				excerpt: '\'CREATE DATABASE\' statement should not  be present (case-insensitive)',
-				recommendation: 'Remove these lines',
-			},
-			dropDB: {
-				type: 'error',
-				matcher: /^DROP DATABASE/i,
-				matchHandler: ( lineNum ) => lineNum,
-				results: [],
-				message: 'DROP DATABASE statement',
-				excerpt: '\'DROP DATABASE\' should not be present (case-insensitive)',
-				recommendation: 'Remove these lines',
-			},
-			alterUser: {
-				type: 'error',
-				matcher: /^(ALTER USER|SET PASSWORD)/i,
-				matchHandler: ( lineNum ) => lineNum,
-				results: [],
-				message: 'ALTER USER statement',
-				excerpt: '\'ALTER USER\' should not be present (case-insensitive)',
-				recommendation: 'Remove these lines',
-			},
-			dropTable: {
-				type: 'required',
-				matcher: /^DROP TABLE IF EXISTS `?([a-z0-9_]*)/i,
-				matchHandler: ( lineNum, results ) => results [ 1 ],
-				results: [],
-				message: 'DROP TABLE',
-				excerpt: '\'DROP TABLE IF EXISTS\' should be present (case-insensitive)',
-				recommendation: 'Check import settings to include DROP TABLE statements',
-			},
-			createTable: {
-				type: 'required',
-				matcher: /^CREATE TABLE `?([a-z0-9_]*)/i,
-				matchHandler: ( lineNum, results ) => results [ 1 ],
-				results: [],
-				message: 'CREATE TABLE',
-				excerpt: '\'CREATE TABLE\' should be present (case-insensitive)',
-				recommendation: 'Check import settings to include CREATE TABLE statements',
-			},
-			siteHomeUrl: {
-				type: 'info',
-				matcher: '\'(siteurl|home)\',\\s?\'(.*?)\'',
-				matchHandler: ( lineNum, results ) => results [ 0 ],
-				results: [],
-				message: 'Siteurl/home matches',
-				excerpt: 'Siteurl/home options',
-				recommendation: '',
-			},
-		};
-		let lineNum = 1;
 
 		readInterface.on( 'line', function( line ) {
 			if ( lineNum % 500 === 0 ) {
@@ -119,32 +148,8 @@ command( {
 		readInterface.on( 'close', async function() {
 			log( `Finished processing ${ lineNum } lines.` );
 			console.log( '\n' );
-			for ( const [ key, check ] of Object.entries( checks ) ) {
-				console.log( '🔍', check.excerpt );
-				if ( check.type === 'error' ) {
-					if ( check.results.length > 0 ) {
-						problemsFound += 1;
-						console.error( chalk.red( 'Error:' ), `${ check.message } on line(s) ${ check.results.join( ',' ) }.` );
-						console.error( chalk.yellow( 'Recommendation:' ), `${ check.recommendation }` );
-					} else {
-						console.log( `✅ ${ check.message } was found ${ check.results.length } times.` );
-					}
-				} else if ( check.type === 'required' ) {
-					if ( check.results.length > 0 ) {
-						console.log( `✅ ${ check.message } was found ${ check.results.length } times.` );
-						if ( key === 'createTable' ) {
-							checkTables( check.results );
-						}
-					} else {
-						problemsFound += 1;
-						console.error( chalk.red( 'Error:' ), `${ check.message } was not found.` );
-						console.error( chalk.yellow( 'Recommendation:' ), `${ check.recommendation }` );
-					}
-				} else if ( check.type === 'info' ) {
-					check.results.forEach( item => {
-						console.log( item );
-					} );
-				}
+			for ( const [ type, check ] of Object.entries( checks ) ) {
+				check.outputHandler( check, type );
 				console.log( '' );
 			}
 
