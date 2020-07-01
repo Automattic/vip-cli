6851ed6925065fe42bf3427f455b4a70903fff0e
Loo2 <joanne.lee@automattic.com>
2020-07-01 13:52:43 -0700

    Bring in dependencies

---
+++ src/bin/vip-import-validate-files.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+// @flow
+
+/**
+ * External dependencies
+ */
+import readline from 'readline';
+import chalk from 'chalk';
+import fs from 'fs';
+
+/**
+ * Internal dependencies
+ */
+import command from 'lib/cli/command';
