410f23af210573d1e6293d0a087b31ee1b57d993
Loo2 <joanne.lee@automattic.com>
2020-07-01 18:33:21 -0700

    Add vip import validate files command

--- src/bin/vip-import-validate-files.js
+++ src/bin/vip-import-validate-files.js
@@ -12,3 +12,8 @@ import fs from 'fs';
  * Internal dependencies
  */
 import command from 'lib/cli/command';
+
+command( { requiredArgs: 1, format: true } )
+	.example( 'vip import validate files <file>', 'Validate your media files' )
+	.argv( process.argv, async ( arg, opts ) => {
+	} );
