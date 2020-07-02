41471da02c3949ec634065b34baae6c44964aae9
Loo2 <joanne.lee@automattic.com>
2020-07-01 23:35:29 -0700

    Ensure that media files are stored in an uploads directory

--- src/bin/vip-import-validate-files.js
+++ src/bin/vip-import-validate-files.js
@@ -4,7 +4,8 @@
 /**
  * External dependencies
  */
-import readline from 'readline';
+import url from 'url';
+import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
 
@@ -15,5 +16,18 @@ import command from 'lib/cli/command';
 
 command( { requiredArgs: 1, format: true } )
 	.example( 'vip import validate files <file>', 'Validate your media files' )
-	.argv( process.argv, async ( arg, opts ) => {
+	.argv( process.argv, async ( file, options ) => {
+		// File comes in as an array as part of the args- turn it into a string
+		const fileString = file.join();
+
+		// Then parse the file to its URL parts
+		file = url.parse( fileString );
+		
+		// Extract the path of the file
+		const path = file.path;
+
+		// Media files must be part of an uploads directory
+		if ( path.search( 'uploads' ) === -1 ) {
+			return console.error( 'Media files must be in an `uploads` directory' );
+		}
 	} );
