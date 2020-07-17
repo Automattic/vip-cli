9ee23ed07d0264db4e24d5e32f7b4400ff7dfcd9
Loo2 <joanne.lee@automattic.com>
2020-07-16 17:17:38 -0700

    Move logic to /lib folder to keep code separated and clean

--- src/bin/vip-import-validate-files.js
+++ src/bin/vip-import-validate-files.js
@@ -8,80 +8,19 @@ import url from 'url';
 import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
-import { promisify } from 'util';
 
 /**
  * Internal dependencies
  */
 import command from 'lib/cli/command';
-
-// Promisify to use async/await
-const readDir = promisify( fs.readdir );
-
-// Accepted media file extensions
-const acceptedExtensions = [
-	'jpg', 'jpeg', 'jpe',
-	'gif',
-	'png',
-	'bmp',
-	'tiff', 'tif',
-	'ico',
-	'asf',
-	'asx',
-	'wmv', 'wmx', 'wm',
-	'avi',
-	'divx',
-	'mov',
-	'qt',
-	'mpeg', 'mpg', 'mpe', 'mp4', 'm4v',
-	'ogv',
-	'webm',
-	'mkv',
-	'3gp', '3gpp', '3g2', '3gp2',
-	'txt',
-	'asc',
-	'c', 'cc', 'h',
-	'srt',
-	'csv', 'tsv',
-	'ics',
-	'rtx',
-	'css',
-	'vtt',
-	'dfxp',
-	'mp3',
-	'm4a', 'm4b',
-	'ra',
-	'ram',
-	'wav',
-	'ogg',
-	'oga',
-	'mid', 'midi',
-	'wma',
-	'wax',
-	'mka',
-	'rtf',
-	'js',
-	'pdf',
-	'class',
-	'psd',
-	'xcf',
-	'doc',
-	'pot',
-	'pps',
-	'ppt',
-	'wri',
-	'xla', 'xls', 'xlt', 'xlw',
-	'mdb', 'mpp',
-	'docx', 'docm', 'dotx', 'dotm',
-	'xlsx', 'xlsm', 'xlsb', 'xltx', 'xltm', 'xlam',
-	'pptx', 'pptm', 'ppsx', 'ppsm', 'potx', 'potm', 'ppam',
-	'sldx', 'sldm',
-	'onetoc', ' onetoc2', 'onetmp', 'onepkg', 'oxps',
-	'xps',
-	'odt', 'odp', 'ods', 'odg', 'odc', 'odb', 'odf',
-	'wp', 'wpd',
-	'key', 'numbers', 'pages',
-];
+import { 
+	acceptedExtensions,
+	findNestedDirectories,
+	sanitizeFileName,
+	recommendedFileStructure,
+	logErrorsForInvalidFileTypes,
+	logErrorsForInvalidFilenames 
+} from '../lib/vip-import-validate-files';
 
 command( { requiredArgs: 1, format: true } )
 	.example( 'vip import validate files <file>', 'Validate your media files' )
@@ -94,47 +33,6 @@ command( { requiredArgs: 1, format: true } )
 
 		// Extract the path of the file
 		const filePath = arg.path;
-
-		const recommendedFileStructure = () => {
-			console.log(
-				'Please follow this structure for your media files: \n\n' +
-				chalk.underline( 'Single sites:' ) +
-				chalk.yellow( ' `uploads/year/month/image.png` \n' ) +
-				' e.g.-' + chalk.yellow( '`uploads/2020/06/image.png` \n' ) +
-				chalk.underline( 'Multisites:' ) +
-				chalk.cyan( ' `uploads/sites/siteID/year/month/image.png` \n' ) +
-				' e.g.-' + chalk.cyan( '`uploads/sites/5/2020/06/images.png` \n' )
-			);
-		};
-
-		/* Find nested directories using recursion to validate the folder structure
-		*/
-		const findNestedDirectories = async directory => {
-			let dir, nestedDir;
-
-			try {
-				// Read what's inside the current directory
-				dir = await readDir( directory );
-
-				nestedDir = dir[ 0 ];
-
-				// Once we hit individual media files, stop
-				const regexExtension = /\.\w{3,4}$/;
-				const mediaFiles = regexExtension.test( nestedDir );
-
-				if ( dir !== undefined && mediaFiles ) {
-					return directory;
-				}
-			} catch ( error ) {
-				console.error( chalk.red( '✕' ), ` Error: Unable to read directory: ${ directory }. Reason: ${ error.message }` );
-			}
-
-			// Update the path with the current directory + nested directory
-			const updatedPath = directory + '/' + nestedDir;
-
-			// Use recursion to map out the file structure
-			return findNestedDirectories( updatedPath );
-		};
 
 		// Ensure media files are stored in an `uploads` directory
 		if ( folder.search( 'uploads' ) === -1 ) {
@@ -186,82 +84,11 @@ command( { requiredArgs: 1, format: true } )
 					errorFileTypes.push( file );
 				}
 
-				/* Character validation
-				* This logic is based on the WordPress core function `sanitize_file_name()`
-				* https://developer.wordpress.org/reference/functions/sanitize_file_name/
-				*/
-				const sanitizeFileName = filename => {
-					let sanitizedFile;
-
-					// Prohibited characters:
-					// Encoded spaces (%20), no-break spaces - keeps words together (\u00A0), and plus signs
-					const regexSpaces = /\u00A0|(%20)|\+/g;
-					sanitizedFile = filename.replace( regexSpaces, ' ' );
-
-					// Prohibited characters:
-					// Special characters: + & # % = ' " \ < > : ; , / ? $ * | ` ! { }
-					const regexSpecialChars = /[\/\'\"\\=<>:;,&?$#*|`!+{}%]/g;
-					sanitizedFile = filename.replace( regexSpecialChars, '' );
-
-					// No dashes, underscores, or periods allowed as the first
-					// or last letter of the file (including the extension)
-					const regexFirstAndLast = /(?:^[\.\-_])|(?:[\.\-_]$)/g;
-					sanitizedFile = filename.replace( regexFirstAndLast, '' );
-
-					// // Check if the file name has been sanitized
-					const checkFile = sanitizedFile === filename;
-
-					return checkFile;
-				};
-
 				// Collect files that have invalid file names to log errors later
 				if ( ! sanitizeFileName( file ) ) {
 					errorFileNames.push( file );
 				}
 			} );
-
-			// Recommend accepted file types
-			const recommendAcceptableFileTypes = () => {
-				console.log(
-					'Accepted file types: \n\n' +
-					chalk.magenta( `${ acceptedExtensions }` )
-				);
-			};
-
-			// Accepted file name characters
-			const recommendAcceptableFileNames = () => {
-				const prohibitedCharacters = '+ & # % = \' \" \ < > : ; , / ? $ * | ` ! { }';
-				const acceptedCharacters = 'Non-English characters, spaces, ( ) [ ] ~';
-
-				console.log(
-					'The following characters are allowed in file names:\n' +
-					chalk.green( `${ acceptedCharacters }` ) + '\n\n' +
-					'The following characters are prohibited in file names:\n' +
-					chalk.red( `${ prohibitedCharacters }` )
-				);
-			};
-
-			// Log errors for files with invalid file extensions and recommend accepted file types
-			const logErrorsForInvalidFileTypes = invalidFiles => {
-				invalidFiles.map( file => {
-					console.error( chalk.red( '✕' ), `File extensions: Invalid file type for file: ${ file }` );
-				} );
-
-				console.log();
-				recommendAcceptableFileTypes();
-				console.log();
-			};
-
-			// Log errors for files with invalid filenames and show a list of accepted/prohibited chars
-			const logErrorsForInvalidFilenames = invalidFiles => {
-				invalidFiles.map( file => {
-					console.log( chalk.red( '✕' ), `Character validation: Invalid filename for file: ${ file }` );
-				} );
-
-				console.log();
-				recommendAcceptableFileNames();
-				console.log();
-			};
 
 			if ( errorFileTypes.length > 0 ) {
 				logErrorsForInvalidFileTypes( errorFileTypes );
---
+++ src/lib/vip-import-validate-files.js
@@ -0,0 +1,197 @@
+import chalk from 'chalk';
+import fs from 'fs';
+import { promisify } from 'util';
+
+// Promisify to use async/await
+const readDir = promisify( fs.readdir );
+
+// Accepted media file extensions
+export const acceptedExtensions = [
+	'jpg', 'jpeg', 'jpe',
+	'gif',
+	'png',
+	'bmp',
+	'tiff', 'tif',
+	'ico',
+	'asf',
+	'asx',
+	'wmv', 'wmx', 'wm',
+	'avi',
+	'divx',
+	'mov',
+	'qt',
+	'mpeg', 'mpg', 'mpe', 'mp4', 'm4v',
+	'ogv',
+	'webm',
+	'mkv',
+	'3gp', '3gpp', '3g2', '3gp2',
+	'txt',
+	'asc',
+	'c', 'cc', 'h',
+	'srt',
+	'csv', 'tsv',
+	'ics',
+	'rtx',
+	'css',
+	'vtt',
+	'dfxp',
+	'mp3',
+	'm4a', 'm4b',
+	'ra',
+	'ram',
+	'wav',
+	'ogg',
+	'oga',
+	'mid', 'midi',
+	'wma',
+	'wax',
+	'mka',
+	'rtf',
+	'js',
+	'pdf',
+	'class',
+	'psd',
+	'xcf',
+	'doc',
+	'pot',
+	'pps',
+	'ppt',
+	'wri',
+	'xla', 'xls', 'xlt', 'xlw',
+	'mdb', 'mpp',
+	'docx', 'docm', 'dotx', 'dotm',
+	'xlsx', 'xlsm', 'xlsb', 'xltx', 'xltm', 'xlam',
+	'pptx', 'pptm', 'ppsx', 'ppsm', 'potx', 'potm', 'ppam',
+	'sldx', 'sldm',
+	'onetoc', ' onetoc2', 'onetmp', 'onepkg', 'oxps',
+	'xps',
+	'odt', 'odp', 'ods', 'odg', 'odc', 'odb', 'odf',
+	'wp', 'wpd',
+	'key', 'numbers', 'pages',
+];
+
+/** Folder structure validation
+ * 
+ * Find nested directories using recursion to validate the folder structure
+ */
+export const findNestedDirectories = async directory => {
+    let dir, nestedDir;
+
+    try {
+        // Read what's inside the current directory
+        dir = await readDir( directory );
+
+        nestedDir = dir[ 0 ];
+
+        // Once we hit individual media files, stop
+        const regexExtension = /\.\w{3,4}$/;
+        const mediaFiles = regexExtension.test( nestedDir );
+
+        if ( dir !== undefined && mediaFiles ) {
+            return directory;
+        }
+    } catch ( error ) {
+        console.error( chalk.red( '✕' ), ` Error: Unable to read directory: ${ directory }. Reason: ${ error.message }` );
+    }
+
+    // Update the path with the current directory + nested directory
+    const updatedPath = directory + '/' + nestedDir;
+
+    // Use recursion to map out the file structure
+    return findNestedDirectories( updatedPath );
+};
+
+/** Character validation
+ * 
+ * This logic is based on the WordPress core function `sanitize_file_name()`
+ * https://developer.wordpress.org/reference/functions/sanitize_file_name/
+ */
+export const sanitizeFileName = filename => {
+    let sanitizedFile;
+
+    // Prohibited characters:
+    // Encoded spaces (%20), no-break spaces - keeps words together (\u00A0), and plus signs
+    const regexSpaces = /\u00A0|(%20)|\+/g;
+    sanitizedFile = filename.replace( regexSpaces, ' ' );
+
+    // Prohibited characters:
+    // Special characters: + & # % = ' " \ < > : ; , / ? $ * | ` ! { }
+    const regexSpecialChars = /[\/\'\"\\=<>:;,&?$#*|`!+{}%]/g;
+    sanitizedFile = filename.replace( regexSpecialChars, '' );
+
+    // No dashes, underscores, or periods allowed as the first
+    // or last letter of the file (including the extension)
+    const regexFirstAndLast = /(?:^[\.\-_])|(?:[\.\-_]$)/g;
+    sanitizedFile = filename.replace( regexFirstAndLast, '' );
+
+    // // Check if the file name has been sanitized
+    const checkFile = sanitizedFile === filename;
+
+    return checkFile;
+};
+
+export const recommendedFileStructure = () => {
+    console.log(
+        'Please follow this structure for your media files: \n\n' +
+        chalk.underline( 'Single sites:' ) +
+        chalk.yellow( ' `uploads/year/month/image.png` \n' ) +
+        ' e.g.-' + chalk.yellow( '`uploads/2020/06/image.png` \n' ) +
+        chalk.underline( 'Multisites:' ) +
+        chalk.cyan( ' `uploads/sites/siteID/year/month/image.png` \n' ) +
+        ' e.g.-' + chalk.cyan( '`uploads/sites/5/2020/06/images.png` \n' )
+    );
+};
+
+/** Recommendations
+ * 
+ * Recommend alternatives to invalid folders or files
+ */
+
+// Recommend accepted file types
+const recommendAcceptableFileTypes = () => {
+    console.log(
+        'Accepted file types: \n\n' +
+        chalk.magenta( `${ acceptedExtensions }` )
+    );
+};
+
+// Accepted file name characters
+const recommendAcceptableFileNames = () => {
+    const prohibitedCharacters = '+ & # % = \' \" \ < > : ; , / ? $ * | ` ! { }';
+    const acceptedCharacters = 'Non-English characters, spaces, ( ) [ ] ~';
+
+    console.log(
+        'The following characters are allowed in file names:\n' +
+        chalk.green( `${ acceptedCharacters }` ) + '\n\n' +
+        'The following characters are prohibited in file names:\n' +
+        chalk.red( `${ prohibitedCharacters }` )
+    );
+};
+
+/**
+ * Error logging
+ *
+ * Log errors for invalid folders or files
+ */
+
+// Log errors for files with invalid file extensions and recommend accepted file types
+export const logErrorsForInvalidFileTypes = invalidFiles => {
+    invalidFiles.map( file => {
+        console.error( chalk.red( '✕' ), `File extensions: Invalid file type for file: ${ file }` );
+    } );
+
+    console.log();
+    recommendAcceptableFileTypes();
+    console.log();
+};
+
+// Log errors for files with invalid filenames and show a list of accepted/prohibited chars
+export const logErrorsForInvalidFilenames = invalidFiles => {
+    invalidFiles.map( file => {
+        console.log( chalk.red( '✕' ), `Character validation: Invalid filename for file: ${ file }` );
+    } );
+
+    console.log();
+    recommendAcceptableFileNames();
+    console.log();
+};
