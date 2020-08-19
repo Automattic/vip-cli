fe84abd9431f3e3763037a1b27a11e0d9771d972
Jeff Bowen <jblz@users.noreply.github.com>
2020-08-19 11:06:14 -0400

    Use babel preset-env & add support for optional chaining / nullish coalescing

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
 module.exports = {
 	presets: [
+		[
+			'@babel/preset-env',
+			{
+				targets: {
+					node: '8.9.0', // Keep this in sync with package.json engines.node
+				},
+			},
+		],
 		'@babel/preset-flow',
 	],
 	plugins: [
