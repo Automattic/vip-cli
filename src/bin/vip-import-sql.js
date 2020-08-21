809562cd732116e0661769f71a09639a0445e5e8
Joanne Lee <joanne.lee@automattic.com>
2020-08-21 13:34:06 -0700

    Add AWS credentials flag options

--- src/bin/vip-import-sql.js
+++ src/bin/vip-import-sql.js
@@ -17,31 +17,34 @@ const appQuery = 'id, name, organization { id, name },environments{ id, appId, t
 command( {
 	appContext: true,
 	appQuery,
-} ).argv( process.argv, async ( arg, opts ) => {
-	const { app } = opts;
-	const { environments, organization } = app;
-	const primaryDomainName = environments[ 0 ].primaryDomain.name;
+} )
+	.option( '--aws-key <key>', 'AWS Key' )
+	.option( '--aws-secret <key>', 'AWS Secret' )
+	.argv( process.argv, async ( arg, opts ) => {
+		const { app } = opts;
+		const { environments, organization } = app;
+		const primaryDomainName = environments[ 0 ].primaryDomain.name;
 
-	console.log( '** Welcome to the WPVIP Site SQL Importer! **\n' );
+		console.log( '** Welcome to the WPVIP Site SQL Importer! **\n' );
 
-	try {
-		supportedAppCheck( app );
-		permissionCheck( app );
-	} catch ( preCheckError ) {
-		console.error( `Import Error: ${ preCheckError }` );
-		return 1;
-	}
+		try {
+			supportedAppCheck( app );
+			permissionCheck( app );
+		} catch ( preCheckError ) {
+			console.error( `Import Error: ${ preCheckError }` );
+			return 1;
+		}
 
-	console.log( 'You are about to import a SQL file to site:' );
-	console.log( `ID: ${ app.id }` );
-	console.log( `Name: ${ app.name }` );
-	console.log( `Primary Domain Name: ${ primaryDomainName }` );
+		console.log( 'You are about to import a SQL file to site:' );
+		console.log( `ID: ${ app.id }` );
+		console.log( `Name: ${ app.name }` );
+		console.log( `Primary Domain Name: ${ primaryDomainName }` );
 
-	try {
-		const uploadURL = await getUploadURL( organization.id, app.id, 'omgwtfbbq.sql' );
-		console.log( await uploadURL.text() );
-	} catch ( e ) {
-		console.error( `Error getting upload URL: ${ e }` );
-		return 1;
-	}
+		try {
+			const uploadURL = await getUploadURL( organization.id, app.id, 'omgwtfbbq.sql' );
+			console.log( await uploadURL.text() );
+		} catch ( e ) {
+			console.error( `Error getting upload URL: ${ e }` );
+			return 1;
+		}
 } );
