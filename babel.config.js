8c3e79d11b197e649a3a0d3f4e02e82f857668c4
Jeff Bowen <jblz@users.noreply.github.com>
2021-01-06 17:28:01 -0500

    bump node minimum version & target to 10

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
 			'@babel/preset-env',
 			{
 				targets: {
-					node: '8.9.0', // Keep this in sync with package.json engines.node
+					node: '10', // Keep this in sync with package.json engines.node
 				},
 			},
 		],
