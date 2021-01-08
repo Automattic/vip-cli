f4845d1f3badf866b2db174d207c9491cddd2d76
Mark Owsiak <mark.owsiak@automattic.com>
2021-01-08 10:14:57 -0500

    Handle linting errors

--- __tests__/lib/validations/sql.js
+++ __tests__/lib/validations/sql.js
@@ -45,31 +45,31 @@ describe( 'lib/validations/sql', () => {
 		it( 'an error, and exits with code 1', () => {
 			expect( mockExit ).toHaveBeenCalledWith( ERROR_CODE );
 		} );
-		it( 'USE statements', () => {
+		it( 'instances of USE statements', () => {
 			expect( output ).toContain( 'USE statement on line(s) 3' );
 		} );
-		it( 'CREATE DATABASE statements', () => {
+		it( 'instances of CREATE DATABASE statements', () => {
 			expect( output ).toContain( 'CREATE DATABASE statement on line(s) 1' );
 		} );
-		it( 'TRIGGER statements', () => {
+		it( 'instances of TRIGGER statements', () => {
 			expect( output ).toContain( 'TRIGGER statement on line(s) 16' );
 		} );
-		it( 'DROP DATABASE statements', () => {
+		it( 'instances of DROP DATABASE statements', () => {
 			expect( output ).toContain( 'DROP DATABASE statement on line(s) 27' );
 		} );
-		it( 'ALTER USER statements', () => {
+		it( 'instances of ALTER USER statements', () => {
 			expect( output ).toContain( 'ALTER USER statement on line(s) 25' );
 		} );
-		it( 'no DROP TABLE IF EXISTS statements', () => {
+		it( 'no instances of DROP TABLE IF EXISTS statements', () => {
 			expect( output ).toContain( 'DROP TABLE was not found' );
 		} );
-		it( 'no CREATE TABLE statements', () => {
+		it( 'no instances of CREATE TABLE statements', () => {
 			expect( output ).toContain( 'CREATE TABLE was not found' );
 		} );
 		it.skip( 'siteurl/home matches', () => {
 			// TODO: the validator should warn when these fields are mismatched
 		} );
-		it( 'ENGINE != InnoDB', () => {
+		it( 'instances of ENGINE != InnoDB', () => {
 			expect( output ).toContain( 'ENGINE != InnoDB on line(s) 14' );
 		} );
 	} );
