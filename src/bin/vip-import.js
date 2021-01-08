b90c569f92efebb34b17f744cca9efdc19bb1606
Mohammad Jangda <batmoo@gmail.com>
2021-01-07 23:09:30 -0500

    Fix validate (sql|files)
    Need to hyphenate the subcommand so that the file is called correctly.
    Otherwise we need to add another file called vip-import-validate.js
    which is another stub like vip-import.js which seems like a bug waiting
    to happen.

--- src/bin/vip-import.js
+++ src/bin/vip-import.js
@@ -12,8 +12,8 @@ import { trackEvent } from 'lib/tracker';
 
 command( )
 	.command( 'sql', 'Import SQL to your database from a file' )
-	.command( 'validate sql', 'Validate your SQL dump' )
-	.command( 'validate files', 'Validate your media file library' )
+	.command( 'validate-sql', 'Validate your SQL dump' )
+	.command( 'validate-files', 'Validate your media file library' )
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_import_command_execute' );
 	} );
