3b0eed139a4620370680d40a440c25b4fd2d1480
Loo2 <joanne.lee@automattic.com>
2021-01-18 19:14:54 -0800

    Fix and add wording for examples and options

--- src/bin/vip-search-replace.js
+++ src/bin/vip-search-replace.js
@@ -23,7 +23,8 @@ const examples = [
 	// `search-replace` flag
 	{
 		usage:'vip search replace <file.sql> --search-replace="<from,to>"',
-		description:'Replace instances of <from> with <to> in the provided <file.sql>',
+		description:'Replace instances of <from> with <to> in the provided <file.sql>\n' +
+		'       * Ensure there are no spaces between your search-replace parameters',
 	},
 	// `in-place` flag
 	{
@@ -33,7 +34,8 @@ const examples = [
 	// `output` flag
 	{
 		usage:'vip search replace <file.sql> --search-replace="<from,to>" --output="<output.sql>"',
-		description:'Search and Replace to the specified output <output.sql> file',
+		description:'Search and Replace to the specified output <output.sql> file\n' +
+		'       * Has no effect when the `in-place` flag is used',
 	}
 ];
 
@@ -42,7 +44,7 @@ command( {
 } )
 	.option( 'search-replace', 'Specify the <from> and <to> pairs to be replaced' )
 	.option( 'in-place', 'Perform the search and replace explicitly on the input file' )
-	.option( 'output', 'Where should the replacement file be written? Has no effect when `in-place` is true. Default: process.stdout' )
+	.option( 'output', 'Specify the replacement output file for Search and Replace', 'process.stdout' )
 	.examples( examples )
 	.argv( process.argv, async ( arg, opt ) => {
 		// TODO: tracks event for usage of this command stand alone
