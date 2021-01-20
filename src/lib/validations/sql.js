502ca5f5ef17254d0009d6d520615c5d708788a1
Joanne Lee <joanne.lee@automattic.com>
2021-01-20 00:12:49 -0800

    Simplify progress log updates for SQL validation for easier user consumption and readability

--- src/lib/validations/sql.js
+++ src/lib/validations/sql.js
@@ -15,7 +15,6 @@ import { stdout as log } from 'single-line-log';
  * Internal dependencies
  */
 import { trackEvent } from 'lib/tracker';
-import { confirm } from 'lib/cli/prompt';
 
 let problemsFound = 0;
 let lineNum = 1;
@@ -196,7 +195,45 @@ function openFile( filename, flags = 'r', mode = 666 ) {
 	} );
 }
 
+// Progress spinner
+const sprite = {
+ i: 0,
+ sprite: [ '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' ],
+ next() {
+  this.i++;
+
+  if ( this.i >= this.sprite.length ) {
+   this.i = 0;
+  }
+
+  return {
+   value: this.sprite[ this.i ],
+   done: false,
+  };
+ },
+};
+
+// Status signs for progress updates
+const marks = {
+ pending: '○',
+ running: chalk.blueBright( sprite.next().value ),
+ success: chalk.green( '✓' ),
+ failed: chalk.red( '✕' ),
+ unknown: chalk.yellow( '✕' ),
+};
+
+// Progress update logs
+const progress = status => {
+	const action = 'Validating SQL';
+
+	let actionProgress = ` ${ marks[ status ] } ${ action }`;
+
+	stdout( `${ actionProgress }` );
+	console.log();
+};
+
 export const validate = async ( filename: string, isImport: boolean = false ) => {
+	progress( 'running' );
 	await trackEvent( 'import_validate_sql_command_execute', { is_import: isImport } );
 
 	let fd;
@@ -206,6 +243,7 @@ export const validate = async ( filename: string, isImport: boolean = false ) =>
 	} catch ( e ) {
 		console.log( chalk.red( 'Error: ' ) + 'The file at the provided path is either missing or not readable.' );
 		console.log( 'Please check the input and try again.' );
+		progress( 'failed' );
 		process.exit( 1 );
 	}
 
@@ -254,32 +292,15 @@ export const validate = async ( filename: string, isImport: boolean = false ) =>
 			console.log();
 		}
 
+		progress( 'failed' );
 		await trackEvent( 'import_validate_sql_command_failure', { is_import: isImport, error: errorSummary } );
 		return process.exit( 1 );
 	}
 
+	progress( 'success' );
 	await trackEvent( 'import_validate_sql_command_success', { is_import: isImport } );
 
 	readInterface.close();
-
-	if ( isImport ) {
-		// Add a confirmation step before running the import
-		const yes = await confirm(
-			[], 'Are you sure you want to continue with the import?'
-		);
-
-		// Bail if user does not wish to proceed
-		if ( ! yes ) {
-			console.log( `${ chalk.red( 'Exiting' ) }` );
-
-			await trackEvent( 'import_continue_cancelled', { is_import: isImport, import_file: filename } );
-
-			process.exit();
-		}
-
-		console.log( `\n${ chalk.underline( 'Starting the import process...' ) }` );
-		return;
-	}
 
 	console.log( '\n🎉 You can now submit for import, see here for more details: ' +
 		'https://docs.wpvip.com/how-tos/prepare-for-site-launch/migrate-content-databases/' );
