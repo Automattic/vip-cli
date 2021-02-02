c78ce5109589caa4bbae0c7450288fc8b79f57aa
Mohammad Jangda <batmoo@gmail.com>
2021-02-01 20:23:03 -0500

    remove unused var

--- src/lib/cli/exit.js
+++ src/lib/cli/exit.js
@@ -13,8 +13,6 @@ import chalk from 'chalk';
  */
 
 export function withError( message: string ) {
-	let updatedMessage = message;
-
 	console.log( message.toString().replace( /^(Error: )*/, chalk.red( 'Error: ' ) ) );
 	process.exit( 1 );
 }
