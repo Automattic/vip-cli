714d774edbc10e62bb84ea1722a11edea54858ff
Gabriel Sambarino <gabriel.sambarino@automattic.com>
2021-03-29 16:08:42 +0200

    Make status a subdommand of `vip import media`

--- src/bin/vip-import.js
+++ src/bin/vip-import.js
@@ -15,14 +15,7 @@ command( )
 	.command( 'validate-sql', 'Validate your SQL dump' )
 	.command( 'validate-files', 'Validate your media file library' )
 	.example( 'vip import sql @mysite.develop <file.sql>', 'Import the given SQL file to your site' )
+	.command( 'media', 'Import media files to your application from a compressed archive' )
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_import_command_execute' );
-	} );
-
-command( )
-	.command( 'media', 'Import media files to your application from a compressed archive' )
-	.command( 'media-progress', 'Import media files to your application from a compressed archive' )
-	.example( 'vip import media @mysite.develop <file.zip>', 'Import media files contained into the archive to your site' )
-	.argv( process.argv, async () => {
-		await trackEvent( 'vip_media_import_command_execute' );
 	} );
