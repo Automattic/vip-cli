e1cfbc6b21e9768591a9537e57bcb1d193714362
Hanif Norman <hanifnorman@gmail.com>
2021-04-01 16:54:46 +0800

    Add start media import command

--- src/bin/vip-import-media.js
+++ src/bin/vip-import-media.js
@@ -8,12 +8,15 @@
 /**
  * External dependencies
  */
+import gql from 'graphql-tag';
 import debugLib from 'debug';
+import chalk from 'chalk';
 
 /**
  * Internal dependencies
  */
 import command from 'lib/cli/command';
+import API from 'lib/api';
 import {
 	currentUserCanImportForApp,
 	isSupportedApp,
@@ -23,6 +26,8 @@ import {
 // eslint-disable-next-line no-duplicate-imports
 import { trackEventWithEnv } from 'lib/tracker';
 import { formatEnvironment, formatSearchReplaceValues, getGlyphForStatus } from 'lib/cli/format';
+import { MediaImportProgressTracker } from 'lib/media-import/progress';
+import { mediaImportCheckStatus } from '../lib/media-import/status';
 
 export type WPSiteListType = {
 	id: string,
@@ -50,15 +55,31 @@ const appQuery = `
 	}
 `;
 
+const START_IMPORT_MUTATION = gql`
+	mutation StartMediaImport($input:AppEnvironmentStartMediaImportInput) {
+		startMediaImport(input: $input) {
+			applicationId
+			environmentId
+			mediaImportStatus {
+				importId
+				siteId
+				status
+				filesTotal
+				filesProcessed
+			}
+		}
+	}
+`;
+
 const debug = debugLib( 'vip:vip-import-media' );
 
 // Command examples for the `vip import media` help prompt
 const examples = [
 	// `media status` subcommand
 	{
-		usage: 'vip import media status @mysite.develop',
+		usage: 'vip import media @mysite.develop --url https://domain.to/archive.zip',
 		description:
-			'Check the status of the most recent import. If an import is running, this will poll until it is complete.',
+			'Start a media import with the contents of the archive file in the URL',
 	},
 ];
 
@@ -66,15 +87,67 @@ command( {
 	appContext: true,
 	appQuery,
 	envContext: true,
-	requiredArgs: 1,
 	module: 'import-media',
-	requireConfirm: 'Are you sure you want to import the contents of the provided file?',
-	skipConfirmPrompt: true,
+	requireConfirm: 'Are you sure you want to import the contents of the url?',
 } )
-	.command( 'status', 'Check the status of the current running import' )
+	// .command( 'status', 'Check the status of the current running import' )
+	.option( 'url', 'Valid URL to download a file archive from', '' )
 	.examples( examples )
 	.argv( process.argv, async ( args: string[], opts ) => {
+		const { app, env, url } = opts;
+
+		if ( ! url ) {
+			console.log( chalk.red( 'Error:' ), 'No URL provided' );
+			return;
+		}
+
+		const track = trackEventWithEnv.bind( null, app.id, env.id );
+		const api = await API();
+
 		debug( 'Options: ', opts );
 		debug( 'Args:', args );
+
+		await track( 'import_media_start_execute' );
+
+		const progressTracker = new MediaImportProgressTracker( [] );
+		progressTracker.prefix = `
+=============================================================
+Starting Media Import for your environment...
+`;
+
+		console.log();
+		console.log( `importing: ${ url }` );
+		console.log( `to: ${ env.primaryDomain.name }` );
+		console.log( `site: ${ env.name } (${ env.type })` );
+		console.log();
+
+		try {
+			const response = await api
+				.mutate( {
+					mutation: START_IMPORT_MUTATION,
+					variables: {
+						input: {
+							applicationId: app.id,
+							environmentId: env.id,
+							archiveUrl: url,
+						},
+					},
+				} );
+			console.log( response );
+
+			await mediaImportCheckStatus( { app, env, progressTracker } );
+		} catch ( e ) {
+			if ( e.graphQLErrors ) {
+				for ( const err of e.graphQLErrors ) {
+					console.log( chalk.red( 'Error:' ), err.message );
+				}
+
+				return;
+			}
+
+			await track( 'import_media_start_execute_error', {
+				error: `Error starting: ${ e.message }`,
+			} );
+		}
 	} );
 
