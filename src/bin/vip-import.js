3a0e17acc24723decd32654e4bcc0c5649398c1d
Sarosh Aga <sarosh.aga@automattic.com>
2021-04-08 15:30:18 +0530

    FORNO-557: Fixing Import Media command
    The subcommand was not defined in the correct position

--- src/bin/vip-import.js
+++ src/bin/vip-import.js
@@ -14,8 +14,8 @@ command( )
 	.command( 'sql', 'Import SQL to your database from a file' )
 	.command( 'validate-sql', 'Validate your SQL dump' )
 	.command( 'validate-files', 'Validate your media file library' )
-	.example( 'vip import sql @mysite.develop <file.sql>', 'Import the given SQL file to your site' )
 	.command( 'media', 'Import media files to your application from a compressed archive' )
+	.example( 'vip import sql @mysite.develop <file.sql>', 'Import the given SQL file to your site' )
 	.example( 'vip import media @mysite.develop https://<path_to_publicly_accessible_archive>', 'Import contents of the given archive file into the media library of your site' )
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_import_command_execute' );
