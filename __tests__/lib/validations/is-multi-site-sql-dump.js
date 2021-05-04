317450fe5ba042a5d745d30f895ceb4c2d15c028
Mark Owsiak <mark.owsiak@automattic.com>
2021-05-04 15:15:00 -0400

    Ensure we check if the columns of the wp_users table have a spam or deleted column to determine if multisite

--- __tests__/lib/validations/is-multi-site-sql-dump.js
+++ __tests__/lib/validations/is-multi-site-sql-dump.js
@@ -22,5 +22,9 @@ describe( 'is-multi-site-sql-dump', () => {
 		it( 'returns false for non-multi site table creations', () => {
 			expect( sqlDumpLineIsMultiSite( 'CREATE TABLE wp_posts' ) ).toBeFalsy();
 		} );
+		it( 'return true if a wp_users table has a spam or deleted column', () => {
+			expect( sqlDumpLineIsMultiSite( '`spam` tinyint(2) NOT NULL DEFAULT 0,' ) ).toBeTruthy();
+			expect( sqlDumpLineIsMultiSite( '`deleted` tinyint(2) NOT NULL DEFAULT 0,' ) ).toBeTruthy();
+		} );
 	} );
 } );
