460e7a3ff63420374f22aa36e8df14b637c3beca
chriszarate <chris@zarate.org>
2021-05-11 14:08:01 -0400

    Make error output consistent and include debug information

--- src/lib/cli/exit.js
+++ src/lib/cli/exit.js
@@ -11,8 +11,15 @@ import chalk from 'chalk';
 /**
  * Internal dependencies
  */
+import env from 'lib/env';
 
 export function withError( message: string ) {
-	console.log( message.toString().replace( /^(Error: )*/, chalk.red( 'Error: ' ) ) );
+	console.log( `${ chalk.red( 'Error: ' ) } ${ message.toString().replace( /^Error:\s*/, '' ) }` );
+
+	// Debug ouput is printed below error output both for information
+	// hierarchy and to make it more likely that the user copies it to their
+	// clipboard when dragging across output.
+	console.log( `${ chalk.yellow( 'Debug: ' ) } VIP-CLI v${ env.app.version }, Node ${ env.node.version }, ${ env.os.name } ${ env.os.version }` );
+
 	process.exit( 1 );
 }
