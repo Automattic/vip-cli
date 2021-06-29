5cb029643b838fe364a5883a01c624549d4ec82f
Sarosh Aga <sarosh.aga@automattic.com>
2021-06-29 15:30:24 +0400

    Fixing dependant tests

--- __tests__/lib/validations/sql.js
+++ __tests__/lib/validations/sql.js
@@ -13,6 +13,7 @@ import fetch, { Response } from 'node-fetch';
  * Internal dependencies
  */
 import { validate } from 'lib/validations/sql';
+import * as apiConfig from 'lib/cli/apiConfig';
 
 const debug = debugLib( '@automattic/vip:__tests__:validations:sql' );
 
@@ -29,6 +30,8 @@ const ERROR_CODE = 1;
 
 jest.mock( 'node-fetch' );
 fetch.mockReturnValue( Promise.resolve( new Response( 'ok' ) ) );
+
+jest.spyOn( apiConfig, 'checkIfUserIsVip' ).mockResolvedValue( true );
 
 describe( 'lib/validations/sql', () => {
 	describe( 'it fails when the SQL has (using bad-sql-dump.sql)', () => {
