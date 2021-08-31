910349adb06ef8bc05137fe3962ad801d7d6cba2
Pau Argelaguet <pau@outlook.com>
2021-08-31 11:30:18 +0200

    Updating help of parent command

--- src/bin/vip-dev-env-import.js
+++ src/bin/vip-dev-env-import.js
@@ -19,12 +19,12 @@ import { DEV_ENVIRONMENT_FULL_COMMAND } from 'lib/constants/dev-environment';
 // Command examples
 const examples = [
 	{
-		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } import db file.sql`,
-		description: 'Use dev-environment to run `wp post list`',
+		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } import sql file.sql`,
+		description: 'Import the given SQL file to your site',
 	},
 	{
-		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } import media`,
-		description: 'Use dev-environment "my_site" to run interactive wp shell',
+		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } import media https://<path_to_publicly_accessible_archive>`,
+		description: 'Import contents of the given archive file into the media library of your site',
 	},
 ];
 
