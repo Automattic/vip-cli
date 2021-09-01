8b7af5d7e970d1ec5d71658465dd3310af0ad348
Pau Argelaguet <pau@outlook.com>
2021-09-01 10:34:38 +0200

    Updating command descriptions on CLI

--- src/bin/vip-dev-env.js
+++ src/bin/vip-dev-env.js
@@ -17,10 +17,10 @@ import command from 'lib/cli/command';
 command( {
 	requiredArgs: 1,
 } )
-	.command( 'create', 'Create a local dev environment' )
+	.command( 'create', 'Create a new local dev environment' )
 	.command( 'start', 'Start a local dev environment' )
 	.command( 'stop', 'Stop a local dev environment' )
-	.command( 'destroy', 'Destroy a local dev environment' )
-	.command( 'info', 'Provides basic info about a local dev environment' )
-	.command( 'exec', 'Execute operation on dev environment' )
+	.command( 'destroy', 'Remove containers, networks, volumes and configuration files of a local dev environment' )
+	.command( 'info', 'Provides basic info about one or multiple local dev environments' )
+	.command( 'exec', 'Execute an operation on a dev environment' )
 	.argv( process.argv );
