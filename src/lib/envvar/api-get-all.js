ba442ba7cff8a7c0ab1f1e60597d0ab4f53f7b35
chriszarate <chris.zarate@automattic.com>
2021-09-21 10:30:18 -0400

    Add `config envvar get` and `get-all` commands

---
+++ src/lib/envvar/api-get-all.js
@@ -0,0 +1,50 @@
+// @flow
+
+/**
+ * External dependencies
+ */
+import gql from 'graphql-tag';
+
+/**
+ * Internal dependencies
+ */
+import API from 'lib/api';
+
+const query = gql`
+	query GetEnvironmentVariablesWithValues(
+		$appId: Int!
+		$envId: Int!
+	) {
+		app(
+			id: $appId
+		) {
+			id
+			environments(
+				id: $envId
+			) {
+				id
+				environmentVariables {
+					total
+					nodes {
+						name
+						# value
+					}
+				}
+			}
+		}
+	}
+`;
+
+export default async function getEnvVars( appId: number, envId: number ) {
+	const api = await API();
+
+	const variables = {
+		appId,
+		envId,
+	};
+
+	const { data } = await api.query( { query, variables } );
+
+	return data.app.environments[ 0 ].environmentVariables.nodes;
+}
+
