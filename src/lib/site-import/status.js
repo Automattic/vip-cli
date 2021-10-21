498e47f7f3347dda6d429a51cd0f6f92703df171
Nejc Lovrencic <nejc.lovrencic@automattic.com>
2021-10-21 14:28:02 +0200

    FORNO-942: Dont mark import as failed until restore has completed

--- src/lib/site-import/status.js
+++ src/lib/site-import/status.js
@@ -262,7 +262,7 @@ ${ maybeExitPrompt }
 							};
 						} );
 
-						if ( statusSteps.some( ( { result } ) => result === 'failed' ) ) {
+						if ( statusSteps.some( ( { result } ) => result === 'failed' ) && ! statusSteps.find( ( { name, result } ) => name === 'restore_db' && ! result ) ) {
 							jobStatus = 'error';
 						} else 	if ( statusSteps.every( ( { result } ) => result === 'success' ) ) {
 							jobStatus = 'success';
