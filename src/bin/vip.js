f615a38241247f21f422b715f731feca0e02539a
Ahmed <ahmed.el.azzabi@automattic.com>
2021-10-22 11:27:00 +0100

    open config command for all customers

--- src/bin/vip.js
+++ src/bin/vip.js
@@ -38,16 +38,12 @@ const runCmd = async function() {
 			console.log( 'You are successfully logged out.' );
 		} )
 		.command( 'app', 'List and modify your VIP applications' )
+		.command( 'config', 'Set configuration for your VIP applications' );
 		.command( 'dev-env', 'Use local dev-environment' )
 		.command( 'import', 'Import media or SQL files into your VIP applications' )
 		.command( 'search-replace', 'Perform search and replace tasks on files' )
 		.command( 'sync', 'Sync production to a development environment' )
 		.command( 'wp', 'Run WP CLI commands against an environment' );
-
-	// Gate access to VIP staff.
-	if ( await checkIfUserIsVip() ) {
-		cmd.command( 'config', 'Set configuration for your VIP applications' );
-	}
 
 	cmd.argv( process.argv );
 };
