9734b0eaae0b777ca486ed943141052581dc2a54
Roberson Gomes <roberson.gomes@automattic.com>
2021-11-17 22:49:27 -0300

    Add `vip logs` command to get the most recent logs from a site

---
+++ __tests__/lib/logs/logs.js
@@ -0,0 +1,74 @@
+/**
+ * External dependencies
+ */
+import gql from 'graphql-tag';
+
+/**
+ * Internal dependencies
+ */
+import API from 'lib/api';
+import { getRecentLogs } from 'lib/logs/logs';
+
+jest.mock( 'lib/api', () => jest.fn() );
+
+const EXPECTED_QUERY = gql`
+	query App( $appId: Int, $envId: Int ) {
+		app( id: $appId ) {
+			environments( id: $envId ) {
+				id
+				logs( type: String, limit: Int ) {
+					nodes {
+						timestamp
+						message
+					}
+				}
+			}
+		}
+	}
+`;
+
+describe( 'getRecentLogs()', () => {
+	beforeEach( jest.clearAllMocks );
+
+	it( 'should query the API with the correct values', async () => {
+		const queryMock = jest.fn();
+
+		API.mockImplementation( () => ( {
+			query: queryMock,
+		} ) );
+
+		queryMock.mockImplementation( () => logsResponse( [
+			{ timestamp: '2021-11-05T20:18:36.234041811Z', message: 'My container message 1' },
+			{ timestamp: '2021-11-09T20:47:07.301221112Z', message: 'My container message 2' },
+		] ) );
+
+		await getRecentLogs( 1, 3, 'batch', 1200 );
+
+		expect( queryMock ).toHaveBeenCalledTimes( 1 );
+		expect( queryMock ).toHaveBeenCalledWith( {
+			query: EXPECTED_QUERY,
+			variables: {
+				appId: 1,
+				envId: 3,
+				type: 'batch',
+				limit: 1200,
+			},
+		} );
+	} );
+} );
+
+function logsResponse( logs ) {
+	return {
+		data: {
+			app: {
+				environments: [
+					{
+						logs: {
+							nodes: logs,
+						},
+					},
+				],
+			},
+		},
+	};
+}
---
+++ src/lib/logs/logs.js
@@ -0,0 +1,52 @@
+/**
+ * @flow
+ * @format
+ */
+
+/**
+ * External dependencies
+ */
+import gql from 'graphql-tag';
+
+/**
+ * Internal dependencies
+ */
+import API from 'lib/api';
+
+const QUERY_ENVIRONMENT_LOGS = gql`
+	query App( $appId: Int, $envId: Int ) {
+		app( id: $appId ) {
+			environments( id: $envId ) {
+				id
+				logs( type: String, limit: Int ) {
+					nodes {
+						timestamp
+						message
+					}
+				}
+			}
+		}
+	}
+`;
+
+export async function getRecentLogs( appId: number, envId: number, type: string, limit: number ): Promise<Array<{ timestamp: string, message: string }>> {
+	const api = await API();
+
+	const response = await api.query( {
+		query: QUERY_ENVIRONMENT_LOGS,
+		variables: {
+			appId,
+			envId,
+			type,
+			limit,
+		},
+	} );
+
+	const logs = response?.data?.app?.environments[ 0 ]?.logs?.nodes;
+
+	if ( ! logs ) {
+		throw new Error( 'Unable to query logs' );
+	}
+
+	return logs;
+}
