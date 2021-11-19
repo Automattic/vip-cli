19c2c8ec68e99a5c666a05a9f827a4b04aaab156
Pavel Schoffer <pavel.schoffer@automattic.com>
2021-11-19 10:50:57 +0100

    Handles user already exists better

--- src/bin/vip-dev-env-import-sql.js
+++ src/bin/vip-dev-env-import-sql.js
@@ -17,6 +17,7 @@ import command from '../lib/cli/command';
 import { getEnvironmentName, handleCLIException } from '../lib/dev-environment/dev-environment-cli';
 import { exec, resolveImportPath } from '../lib/dev-environment/dev-environment-core';
 import { DEV_ENVIRONMENT_FULL_COMMAND } from '../lib/constants/dev-environment';
+import chalk from 'chalk';
 
 const examples = [
 	{
@@ -62,8 +63,16 @@ command( {
 			const cacheArg = [ 'wp', 'cache', 'flush' ];
 			await exec( slug, cacheArg );
 
-			const addUserArg = [ 'wp', 'user', 'create', 'vipgo', 'vipgo@go-vip.net', '--user_pass=password', '--role=administrator' ];
-			await exec( slug, addUserArg );
+			try {
+				const addUserArg = [ 'wp', 'user', 'create', 'vipgo', 'vipgo@go-vip.net', '--user_pass=password', '--role=administrator' ];
+				await exec( slug, addUserArg );
+			} catch ( exception ) {
+				if ( ( exception.message || '' ).includes( 'is already registered' ) ) {
+					console.log( chalk.bold( chalk.green( 'Success: ' ) ) + 'Skipping user vipgo provisioning' );
+				} else {
+					throw exception;
+				}
+			}
 		} catch ( error ) {
 			handleCLIException( error );
 		}
