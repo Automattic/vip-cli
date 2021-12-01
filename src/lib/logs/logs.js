8fb11e87500b3471af54ff60e1adac2b48a8bf3b
Mohammad Jangda <batmoo@gmail.com>
2021-12-01 13:55:55 -0500

    Throw if no logs found

--- src/lib/logs/logs.js
+++ src/lib/logs/logs.js
@@ -48,5 +48,9 @@ export async function getRecentLogs( appId: number, envId: number, type: string,
 		throw new Error( 'Unable to query logs' );
 	}
 
+	if ( ! logs.length ) {
+		throw new Error( 'No logs found' );
+	}
+
 	return logs;
 }
