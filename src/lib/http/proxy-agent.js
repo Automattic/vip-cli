45ac011f83bb0c16afadf1d39a339a7a3831a202
Frank Cerny <Frank.Cerny@clev.frb.org>
2022-04-04 09:17:52 -0400

    Filling out the rest of the scaffolding (TDDs upcoming)

--- src/lib/http/proxy-agent.js
+++ src/lib/http/proxy-agent.js
@@ -9,28 +9,57 @@ import ProxyAgent from 'proxy-agent';
  * Internal dependencies
  */
 
-export default function createProxyAgent() {
+// Note: This module requires the use of a special environment variable "VIP_PROXY_ENABLED"
+// The setting of it to any value allows this module to create a proxy agent based on proxy environment variables
+// If not set, this module will revert back to the previous functionality (hence being fully backward compatible and non-breaking for users)
+
+// Allow users to use either lower or uppercase versions of proxy variable names (uppercase will get precedence)
+const HTTPS_PROXY = process.env.HTTPS_PROXY | process.env.https_proxy | null
+const HTTP_PROXY = process.env.HTTP_PROXY | process.env.http_proxy | null
+const NO_PROXY = process.env.NO_PROXY | process.env.no_proxy | null
+const VIP_PROXY = process.env.VIP_PROXY | null
+const PROXY_FEATURE_ENABLED = process.env.VIP_PROXY_ENABLED | null
+
+export default function createProxyAgent(url) {
 	// VIP Socks Proxy should take precedence, should be fully backward compatible
-	if ( process.env.VIP_PROXY ) {
-		return new ProxyAgent( process.env.VIP_PROXY );
+	if ( VIP_PROXY ) {
+		return new ProxyAgent( VIP_PROXY );
 	}
-	// HTTPS will take precedence over HTTP
-	if ( process.env.HTTPS_PROXY ) {
-		return new ProxyAgent( process.env.HTTPS_PROXY );
+	else if (PROXY_FEATURE_ENABLED && !CoveredInNoProxy(url) && (HTTPS_PROXY || HTTP_PROXY))
+	{
+		return GetWebProxyAgentBasedOnProtocol(url);
 	}
-	if ( process.env.HTTP_PROXY ) {
-		return new ProxyAgent( process.env.HTTP_PROXY );
-	}
+	// If no environment variables are set, the no proxy is in effect, or if the proxy enable is not set return null (equivilant of no Proxy agent)
 	else {
 		return null;
 	}
-	// TODO - Should we take a URL in and just use it to create the correct proxy agent based on our configuration?
-	// This could break if NO_PROXY is set for these things....
 }
 
 // Determine if a NO_PROXY variable is applicable to a given URL
+// Parameters:
+//	- url (string): absolute desintation URL (including the protocol)
 // Requires: NO_PROXY cannot be empty
-// TODO
-function CoveredInNoProxy() {
+// NO_PROXY Rules:
+//	
+// References:
+//	- Gitlab Article on Standardizing NO_PROXY: https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy/
+function CoveredInNoProxy(url) {
+	// Allow for the use of a single wild card "*" which means nothing should go through the proxy
+}
 
+// Returns either an HTTPS or HTTP ProxyAgent based on the protocol of the given URL
+// Parameters:
+//	- url (string): absolute desintation URL (including the protocol)
+// Requires:
+//	- a no_proxy variable is not applicable to the url
+//	- the url protocol is either https or http
+//	- either the environment variable HTTP_PROXY or HTTPS_PROXY is set
+function GetWebProxyAgentBasedOnProtocol(url) {
+	var protocol = url.substr(0,5)
+	if (protocol.equals("https") && HTTPS_PROXY) {
+		return new ProxyAgent( HTTPS_PROXY )
+	}
+	else if (protocol.equals("http:") && HTTP_PROXY) {
+		return new ProxyAgent( HTTP_PROXY )
+	}
 }
