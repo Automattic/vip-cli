5f4bd04876c950a73c06bc0145512b15afd39dbf
Luiz Tiago Oliveira <luiztiago@gmail.com>
2022-06-14 10:59:55 -0300

    Added errorMessage to trackEvent

--- src/bin/vip-cache-purge-url.js
+++ src/bin/vip-cache-purge-url.js
@@ -47,7 +47,7 @@ export async function cachePurgeCommand( urls = [], opt = {} ): void {
 	}
 
 	if ( ! urls.length ) {
-		await trackEvent( 'cache_purge_url_command_error', trackingParams );
+		await trackEvent( 'cache_purge_url_command_error', { ...trackingParams, error: 'No URL provided' } );
 
 		exit.withError( 'Please supply at least one URL.' );
 	}
