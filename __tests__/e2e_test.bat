9a1fef50a0cf0cafcb058595d3528ba3d66eb7b1
Pavel Schoffer <pavel.schoffer@automattic.com>
2022-08-10 11:40:41 +0200

    e2e test

---
+++ __tests__/e2e_test.bat
@@ -0,0 +1,32 @@
+
+call npm pack
+
+echo "== Installing vip"
+
+FOR /R "." %%f IN ( *.tgz) DO  (
+    call npm i -g %%f
+)
+
+echo "== Running e2e tests"
+
+call vip --help
+
+rem dev-env tests
+
+echo "== Creating dev-env"
+
+call vip dev-env create --client-code image --title Test --multisite false --php 8.0 --wordpress 6.0 --mu-plugins image -e false -p false -x false
+
+if NOT %errorlevel% == 0 (
+    echo "== Failed to create dev-env"
+    exit 1
+)
+
+echo "== Starting dev-env"
+
+call vip dev-env start
+
+if NOT %errorlevel% == 0 (
+    echo "== Dev-env failed to start"
+    exit 1
+)
