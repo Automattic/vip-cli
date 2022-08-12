9aedbbe91f3a605abfa8e79b0755ee945d84e968
Pavel Schoffer <pavel.schoffer@automattic.com>
2022-08-12 12:11:30 +0200

    reduce scope

--- __tests__/e2e_test.bat
+++ __tests__/e2e_test.bat
@@ -1,57 +1,22 @@
+call npm pack
 
-@REM call DockerCli -SwitchLinuxEngine
-call docker version
-call where docker
-@REM call dir "C:\Program Files\Docker"
-call where docker-compose
+echo "== Installing vip"
 
-@REM call dir \s DockerCli.exe
-C:
-call dir \s DockerCli.exe
-@REM call dir "C:\ProgramData\Chocolatey"
+FOR /R "." %%f IN ( *.tgz) DO  (
+    call npm i -g %%f
+)
 
-@REM call cat "C:\ProgramData\Docker\config\daemon.json"
+echo "== Running e2e tests"
 
-@REM call Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
-call Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-call Get-PackageProvider -ListAvailableget-packagesource -ProviderName DockerMsftProvider
+call vip --help
 
-call wsl -l -o
+rem dev-env tests
 
-call wsl --status
+echo "== Creating dev-env"
 
-call systeminfo
+call vip dev-env create --client-code image --title Test --multisite false --php 8.0 --wordpress 6.0 --mu-plugins image -e false -p false -x false
 
-call docker pull ubuntu
-
-@REM call npm pack
-
-@REM echo "== Installing vip"
-
-@REM FOR /R "." %%f IN ( *.tgz) DO  (
-@REM     call npm i -g %%f
-@REM )
-
-@REM echo "== Running e2e tests"
-
-@REM call vip --help
-
-@REM rem dev-env tests
-
-@REM echo "== Creating dev-env"
-
-@REM call vip dev-env create --client-code image --title Test --multisite false --php 8.0 --wordpress 6.0 --mu-plugins image -e false -p false -x false
-
-@REM if NOT %errorlevel% == 0 (
-@REM     echo "== Failed to create dev-env"
-@REM     exit 1
-@REM )
-
-@REM echo "== Starting dev-env"
-
-@REM call vip dev-env start --skip-wp-versions-check
-
-@REM if NOT %errorlevel% == 0 (
-@REM     echo "== Dev-env failed to start"
-@REM     exit 1
-@REM )
+if NOT %errorlevel% == 0 (
+    echo "== Failed to create dev-env"
+    exit 1
+)
