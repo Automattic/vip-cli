716ac6d6a391536377ea1a92077e5a781e68fe6b
Pavel Schoffer <pavel.schoffer@automattic.com>
2022-08-15 12:47:34 +0200

    remove @app.env from examples

--- src/bin/vip-dev-env-info.js
+++ src/bin/vip-dev-env-info.js
@@ -17,7 +17,7 @@ import { trackEvent } from 'lib/tracker';
 import command from 'lib/cli/command';
 import { printEnvironmentInfo, printAllEnvironmentsInfo } from 'lib/dev-environment/dev-environment-core';
 import { getEnvironmentName, handleCLIException } from 'lib/dev-environment/dev-environment-cli';
-import { DEV_ENVIRONMENT_FULL_COMMAND, DEV_ENVIRONMENT_SUBCOMMAND } from 'lib/constants/dev-environment';
+import { DEV_ENVIRONMENT_FULL_COMMAND } from 'lib/constants/dev-environment';
 import { getEnvTrackingInfo, validateDependencies } from '../lib/dev-environment/dev-environment-cli';
 
 const debug = debugLib( '@automattic/vip:bin:dev-environment' );
@@ -26,10 +26,6 @@ const examples = [
 	{
 		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } info --all`,
 		description: 'Return information about all local dev environments',
-	},
-	{
-		usage: `vip @123 ${ DEV_ENVIRONMENT_SUBCOMMAND } info`,
-		description: 'Return information about dev environment for site 123',
 	},
 	{
 		usage: `${ DEV_ENVIRONMENT_FULL_COMMAND } info --slug=my_site`,
