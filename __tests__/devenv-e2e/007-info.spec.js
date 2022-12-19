1b392f0866b4948ff223b15738bea9ba563a9a04
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-12-19 09:23:34 +0200

    vip dev-env info

---
+++ __tests__/devenv-e2e/007-info.spec.js
@@ -0,0 +1,101 @@
+/**
+ * External dependencies
+ */
+import { mkdtemp, rm } from 'node:fs/promises';
+import path from 'node:path';
+import os from 'node:os';
+import { describe, expect, it, jest } from '@jest/globals';
+import xdgBaseDir from 'xdg-basedir';
+import Docker from 'dockerode';
+
+/**
+ * Internal dependencies
+ */
+import { CliTest } from './helpers/cli-test';
+import { getProjectSlug, prepareEnvironment } from './helpers/utils';
+import { vipDevEnvCreate, vipDevEnvInfo, vipDevEnvStart } from './helpers/commands';
+import nock from 'nock';
+import { getExistingContainers, killContainersExcept } from './helpers/docker-utils';
+
+jest.setTimeout( 30 * 1000 );
+
+describe( 'vip dev-env info', () => {
+	/** @type {CliTest} */
+	let cliTest;
+	/** @type {NodeJS.ProcessEnv} */
+	let env;
+	/** @type {string} */
+	let tmpPath;
+
+	beforeAll( async () => {
+		nock.cleanAll();
+		nock.enableNetConnect();
+
+		cliTest = new CliTest();
+	} );
+
+	afterAll( () => nock.restore() );
+
+	beforeEach( async () => {
+		tmpPath = await mkdtemp( path.join( os.tmpdir(), 'vip-dev-env-' ) );
+		xdgBaseDir.data = tmpPath;
+
+		env = prepareEnvironment( tmpPath );
+	} );
+
+	afterEach( () => rm( tmpPath, { recursive: true, force: true } ) );
+
+	it( 'should fail on a non-existing environment', async () => {
+		const slug = getProjectSlug();
+		const result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvInfo, '--slug', slug ], { env } );
+		expect( result.rc ).toBeGreaterThan( 0 );
+		expect( result.stderr ).toContain( 'Error: Environment doesn\'t exist.' );
+	} );
+
+	it( 'should display the info', async () => {
+		const slug = getProjectSlug();
+
+		let result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvCreate, '--slug', slug ], { env }, true );
+		expect( result.rc ).toBe( 0 );
+		expect( result.stdout ).toContain( `vip dev-env start --slug ${ slug }` );
+		expect( result.stderr ).toBe( '' );
+
+		result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvInfo, '--slug', slug ], { env }, true );
+		expect( result.rc ).toBe( 0 );
+		expect( result.stdout ).toMatch( new RegExp( `SLUG\\s+${ slug }` ) );
+		expect( result.stdout ).toMatch( /STATUS\s+DOWN/ );
+		[ 'LOCATION', 'SERVICES', 'NGINX URLS', 'LOGIN URL', 'DEFAULT USERNAME', 'DEFAULT PASSWORD' ]
+			.forEach( str => expect( result.stdout ).toContain( str ) );
+	} );
+
+	describe( 'for started environments', () => {
+		/** @type {Docker} */
+		let docker;
+		/** @type {string[]} */
+		let containerIDs;
+
+		beforeAll( async () => {
+			docker = new Docker();
+			containerIDs = await getExistingContainers( docker );
+		} );
+
+		afterEach( () => killContainersExcept( docker, containerIDs ) );
+
+		it( 'should list them as UP', async () => {
+			const slug = getProjectSlug();
+
+			let result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvCreate, '--slug', slug ], { env }, true );
+			expect( result.rc ).toBe( 0 );
+			expect( result.stdout ).toContain( `vip dev-env start --slug ${ slug }` );
+
+			result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvStart, '--slug', slug ], { env }, true );
+			expect( result.rc ).toBe( 0 );
+			expect( result.stdout ).toMatch( /STATUS\s+UP/u );
+
+			result = await cliTest.spawn( [ process.argv[ 0 ], vipDevEnvInfo ], { env }, true );
+			expect( result.rc ).toBe( 0 );
+			expect( result.stdout ).toMatch( new RegExp( `SLUG\\s+${ slug }` ) );
+			expect( result.stdout ).toMatch( /STATUS\s+UP/ );
+		}, 60000 );
+	} );
+} );
