5c7f8cbfbdc176fc71590764c5ec825616e12a84
Katrina Tantay <katrina.tantay@automattic.com>
2022-12-29 16:35:23 -0600

    Added vip prefix to vip user

--- src/lib/tracker.js
+++ src/lib/tracker.js
@@ -15,7 +15,7 @@ import env from './env';
 
 let analytics = null;
 
-async function init(): Promise<Analytics> {
+async function init(): Promise< Analytics > {
 	const uuid = await Token.uuid();
 
 	const clients = [];
@@ -26,11 +26,13 @@ async function init(): Promise<Analytics> {
 	if ( tracksUserType && tracksEventPrefix ) {
 		clients.push( new Tracks( uuid, tracksUserType, tracksEventPrefix, env ) );
 
-		clients.push( new Pendo( {
-			env,
-			eventPrefix: tracksEventPrefix,
-			userId: uuid,
-		} ) );
+		clients.push(
+			new Pendo( {
+				env,
+				eventPrefix: tracksEventPrefix,
+				userId: uuid,
+			} )
+		);
 	}
 
 	analytics = new Analytics( { clients } );
@@ -38,7 +40,7 @@ async function init(): Promise<Analytics> {
 	return analytics;
 }
 
-async function getInstance(): Promise<Analytics> {
+async function getInstance(): Promise< Analytics > {
 	if ( analytics ) {
 		return analytics;
 	}
@@ -48,7 +50,7 @@ async function getInstance(): Promise<Analytics> {
 	return analytics;
 }
 
-export async function trackEvent( ...args ): Promise<Response> {
+export async function trackEvent( ...args ): Promise< Response > {
 	try {
 		await Token.uuid();
 		const client = await getInstance();
@@ -58,10 +60,14 @@ export async function trackEvent( ...args ): Promise<Response> {
 	}
 }
 
-export async function aliasUser( vipUserId ): Promise<Response> {
+export async function aliasUser( vipUserId ): Promise< Response > {
 	try {
 		if ( vipUserId ) {
-			await trackEvent( '_alias_user', { ui: vipUserId, _ut: config.tracksUserType, anonid: Token.uuid() } );
+			await trackEvent( '_alias_user', {
+				ui: `vip-${ vipUserId }`,
+				_ut: config.tracksUserType,
+				anonid: Token.uuid(),
+			} );
 			Token.setUuid( vipUserId );
 		}
 	} catch ( err ) {
@@ -69,6 +75,11 @@ export async function aliasUser( vipUserId ): Promise<Response> {
 	}
 }
 
-export async function trackEventWithEnv( appId, envId, eventName, eventProps = {} ): Promise<Response> {
+export async function trackEventWithEnv(
+	appId,
+	envId,
+	eventName,
+	eventProps = {}
+): Promise< Response > {
 	return trackEvent( eventName, { ...eventProps, app_id: appId, env_id: envId } );
 }
