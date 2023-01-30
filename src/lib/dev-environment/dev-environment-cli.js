f6ce9b1a6782c14a63cfbaa921618d666f72ae64
Ahmed S. Wasif <ahmed.wasif@automattic.com>
2023-01-30 10:12:20 +0600

    Refactor dev-env-import-sql command logic
    We are extracting the logic for reusabilitry. We will use this in the dev-env-sync-sql command.

--- src/lib/dev-environment/dev-environment-cli.js
+++ src/lib/dev-environment/dev-environment-cli.js
@@ -20,6 +20,7 @@ import dns from 'dns';
  */
 import { ProgressTracker } from '../../lib/cli/progress';
 import { trackEvent } from '../tracker';
+import { validate as validateSQL } from '../validations/sql';
 import {
 	DEV_ENVIRONMENT_FULL_COMMAND,
 	DEV_ENVIRONMENT_DEFAULTS,
@@ -28,7 +29,7 @@ import {
 	DEV_ENVIRONMENT_NOT_FOUND,
 	DEV_ENVIRONMENT_PHP_VERSIONS,
 } from '../constants/dev-environment';
-import { getAllEnvironmentNames, getVersionList, readEnvironmentData } from './dev-environment-core';
+import { getAllEnvironmentNames, getEnvironmentPath, getVersionList, readEnvironmentData, resolveImportPath, exec } from './dev-environment-core';
 import type {
 	AppInfo,
 	ComponentConfig,
@@ -37,7 +38,7 @@ import type {
 	InstanceData,
 	WordPressConfig,
 } from './types';
-import { validateDockerInstalled, validateDockerAccess } from './dev-environment-lando';
+import { validateDockerInstalled, validateDockerAccess, bootstrapLando, isEnvUp } from './dev-environment-lando';
 import UserError from '../user-error';
 import typeof Command from '../../lib/cli/command';
 
@@ -692,5 +693,78 @@ export function getEnvTrackingInfo( slug: string ): any {
 		return {
 			slug,
 		};
+	}
+}
+
+export class DevEnvSQLImportCommand {
+	fileName;
+	options;
+	slug;
+	trackingInfo;
+
+	constructor( fileName, options ) {
+		this.fileName = fileName;
+		this.options = options;
+		this.slug = getEnvironmentName( options );
+	}
+
+	async run() {
+		const lando = await bootstrapLando();
+		await validateDependencies( lando, this.slug );
+
+		const { searchReplace, inPlace } = this.options;
+		const resolvedPath = await resolveImportPath( this.slug, this.fileName, searchReplace, inPlace );
+
+		if ( ! this.options.skipValidate ) {
+			if ( ! isEnvUp( lando, getEnvironmentPath( this.slug ) ) ) {
+				throw new UserError( 'Environment needs to be started first' );
+			}
+
+			const expectedDomain = `${ this.slug }.vipdev.lndo.site`;
+			await validateSQL( resolvedPath, {
+				isImport: false,
+				skipChecks: [],
+				extraCheckParams: { siteHomeUrlLando: expectedDomain },
+			} );
+		}
+
+		const fd = await fs.promises.open( resolvedPath, 'r' );
+		const importArg = [ 'db', '--disable-auto-rehash' ];
+		const origIsTTY = process.stdin.isTTY;
+
+		try {
+			/**
+			 * When stdin is a TTY, Lando passes the `--tty` flag to Docker.
+			 * This breaks our code when we pass the stream as stdin to Docker.
+			 * exec() then fails with "the input device is not a TTY".
+			 *
+			 * Therefore, for the things to work, we have to pretend that stdin is not a TTY :-)
+			 */
+			process.stdin.isTTY = false;
+			await exec( lando, this.slug, importArg, { stdio: [ fd, 'pipe', 'pipe' ] } );
+			console.log( `${ chalk.green.bold( 'Success:' ) } Database imported.` );
+		} finally {
+			process.stdin.isTTY = origIsTTY;
+		}
+
+		if ( searchReplace && searchReplace.length && ! inPlace ) {
+			fs.unlinkSync( resolvedPath );
+		}
+
+		const cacheArg = [ 'wp', 'cache', 'flush' ];
+		await exec( lando, this.slug, cacheArg );
+
+		try {
+			await exec( lando, this.slug, [ 'wp', 'cli', 'has-command', 'vip-search' ] );
+			const doIndex = await promptForBoolean( 'Do you want to index data in Elasticsearch (used by Enterprise Search)?', true );
+			if ( doIndex ) {
+				await exec( lando, this.slug, [ 'wp', 'vip-search', 'index', '--setup', '--network-wide', '--skip-confirm' ] );
+			}
+		} catch ( err ) {
+			// Exception means they don't have vip-search enabled.
+		}
+
+		const addUserArg = [ 'wp', 'dev-env-add-admin', '--username=vipgo', '--password=password' ];
+		await exec( lando, this.slug, addUserArg );
 	}
 }
