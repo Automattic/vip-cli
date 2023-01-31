84113ceccf8225828ba0577e9e77d38c74906bcc
Ahmed S. Wasif <ahmed.wasif@automattic.com>
2023-01-31 11:40:09 +0600

    Move makeTempDir function to utils
    We are also making it reusable with custom prefix, and refactoring places where it is already used.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,23 @@
 /**
+ * @flow
+ * @format
+ */
+
+/**
+ * External dependencies
+ */
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import debugLib from 'debug';
+
+/**
+ * Internal dependencies
+ */
+
+const debug = debugLib( '@automattic/vip:lib:search-and-replace' );
+
+/**
  * Polls a function until its return value satisfies a condition
  *
  * @param {Function} fn       A function to poll
@@ -19,4 +38,17 @@ export async function pollUntil( fn, interval, isDone ) {
 		// eslint-disable-next-line no-await-in-loop
 		await new Promise( res => setTimeout( res, interval ) );
 	}
+}
+
+/**
+ * Create a temporary directory in the system's temp directory
+ *
+ * @param {string} prefix Prefix for the directory name
+ * @return {string}				Path to the temporary directory
+ * @throws {Error} 				If the directory cannot be created
+ */
+export function makeTempDir( prefix = 'vip-cli' ) {
+	const tempDir = fs.mkdtempSync( path.join( os.tmpdir(), `${ prefix }-` ) );
+	debug( `Created a directory to hold temporary files: ${ tempDir }` );
+	return tempDir;
 }
