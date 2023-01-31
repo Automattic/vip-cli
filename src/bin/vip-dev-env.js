9e94c97ff5cca80e8cf71885868bf4b6af9a2247
Rinat Khaziev <rinat.khaziev@gmail.com>
2023-01-31 16:31:45 -0600

    Disambiguate exec vs shell in command description

--- src/bin/vip-dev-env.js
+++ src/bin/vip-dev-env.js
@@ -24,7 +24,7 @@ command( {
 	.command( 'destroy', 'Remove containers, networks, volumes and configuration files of a local dev environment' )
 	.command( 'info', 'Provides basic info about one or multiple local dev environments' )
 	.command( 'list', 'Provides basic info about all local dev environments' )
-	.command( 'exec', 'Execute an operation on a dev environment' )
+	.command( 'exec', 'Execute a WP-CLI command in local dev environment' )
 	.command( 'import', 'Import data into a local WordPress environment' )
 	.command( 'shell', 'Spawns a shell in a dev environment' )
 	.argv( process.argv );
