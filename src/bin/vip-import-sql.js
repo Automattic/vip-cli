5ed36b99fbd79e9f98901594850cfab5a8680754
Sarosh Aga <sarosh.aga@automattic.com>
2023-02-03 13:00:45 +0400

    Correcting fileName assignment

--- src/bin/vip-import-sql.js
+++ src/bin/vip-import-sql.js
@@ -418,11 +418,10 @@ command( {
 		const formattedEnvironment = formatEnvironment( opts.env.type );
 		const launched = opts.env.launched;
 
+		let fileNameToUpload = fileName;
+
 		// Extract base file name and exit if it contains unsafe character
 		validateFilename( path.basename( fileName ) );
-
-		// Fetch only the filename from file path
-		let fileNameToUpload = fileName;
 
 		// SQL file validations
 		const tableNames = await validateAndGetTableNames( {
