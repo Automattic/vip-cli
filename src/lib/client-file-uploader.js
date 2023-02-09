Automattic/vip-cli

commit 711f20e6c34b10706f0a7677be030599056dcf61

Remove promisify

src/lib/client-file-uploader.js
@@ -13,10 +13,9 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import { createGunzip, createGzip } from 'zlib';
 import { createHash } from 'crypto';
-import { PassThrough, pipeline } from 'stream';
+import { pipeline, PassThrough } from 'node:stream/promises';
 import { Parser as XmlParser } from 'xml2js';
 import debugLib from 'debug';
-import { promisify } from 'util';
 
 /**
  * Internal dependencies
@@ -109,8 +108,7 @@ export const gzipFile = async ( uncompressedFileName: string, compressedFileName
 export const unzipFile = async ( inputFilename: string, outputFilename: string ) => {
 	const source = fs.createReadStream( inputFilename );
 	const destination = fs.createWriteStream( outputFilename );
-	const pipe = promisify( pipeline );
-	await pipe( source, createGunzip(), destination );
+	await pipeline( source, createGunzip(), destination );
 };
 
 export async function getFileMeta( fileName: string ): Promise<FileMeta> {

commit 571c2d1262fd3255418e0a47370d71a04ffda422

Prepare v2.26.2 (#1278)
* Add ability to override our default options for lando with a global Lando config.yml

* Lint

* Rely upon buildConfig() instead

* Fix the PassThrough class import (#1277)

- bug introduced in 711f20e6c34b10706f0a7677be030599056dcf61

- https://nodejs.org/api/stream.html#class-streampassthrough

---------

Co-authored-by: Rinat Khaziev <rinat.khaziev@gmail.com>
Co-authored-by: Volodymyr Kolesnykov <volodymyr@wildwolf.name>
Co-authored-by: Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>

src/lib/client-file-uploader.js
@@ -13,7 +13,8 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import { createGunzip, createGzip } from 'zlib';
 import { createHash } from 'crypto';
-import { pipeline, PassThrough } from 'node:stream/promises';
+import { pipeline } from 'node:stream/promises';
+import { PassThrough } from 'stream';
 import { Parser as XmlParser } from 'xml2js';
 import debugLib from 'debug';
 

src/lib/dev-environment/dev-environment-lando.js
@@ -9,6 +9,7 @@ import os from 'os';
 import fs from 'fs';
 import path from 'path';
 import Lando from 'lando/lib/lando';
+import { buildConfig } from 'lando/lib/bootstrap';
 import landoUtils from 'lando/plugins/lando-core/lib/utils';
 import landoBuildTask from 'lando/plugins/lando-tooling/lib/build';
 import chalk from 'chalk';
@@ -42,7 +43,7 @@ async function getLandoConfig() {
 	const landoPath = path.join( nodeModulesPath, 'lando' );
 	const atLandoPath = path.join( nodeModulesPath, '@lando' );
 
-	debug( `Getting lando config, using paths '${ landoPath }' and '${ atLandoPath }' for plugins` );
+	debug( `Getting Lando config, using paths '${ landoPath }' and '${ atLandoPath }' for plugins` );
 
 	const isLandoDebugSelected = debugLib.enabled( DEBUG_KEY );
 	const isAllDebugSelected = debugLib.enabled( '"*"' );
@@ -65,8 +66,9 @@ async function getLandoConfig() {
 		// Ignore
 	}
 
-	return {
+	const config = {
 		logLevelConsole,
+		configSources: [ path.join( landoDir, 'config.yml' ) ],
 		landoFile: '.lando.yml',
 		preLandoFiles: [ '.lando.base.yml', '.lando.dist.yml', '.lando.upstream.yml' ],
 		postLandoFiles: [ '.lando.local.yml' ],
@@ -124,6 +126,8 @@ async function getLandoConfig() {
 		domain: 'lndo.site',
 		version: 'unknown',
 	};
+
+	return buildConfig( config );
 }
 
 const appMap: Map<string, App> = new Map();