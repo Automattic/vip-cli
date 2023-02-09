Automattic/vip-cli

commit 711f20e6c34b10706f0a7677be030599056dcf61

Remove promisify

src/lib/client-file-uploader.js
@@ -13,10 +13,9 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import { createGunzip, createGzip } from 'zlib';
 import { createHash } from 'crypto';
-import { PassThrough, pipeline } from 'stream';
+import { pipeline, PassThrough } from 'node:stream/promises';
 import { Parser as XmlParser } from 'xml2js';
 import debugLib from 'debug';
-import { promisify } from 'util';
 
 /**
  * Internal dependencies
@@ -109,8 +108,7 @@ export const gzipFile = async ( uncompressedFileName: string, compressedFileName
 export const unzipFile = async ( inputFilename: string, outputFilename: string ) => {
 	const source = fs.createReadStream( inputFilename );
 	const destination = fs.createWriteStream( outputFilename );
-	const pipe = promisify( pipeline );
-	await pipe( source, createGunzip(), destination );
+	await pipeline( source, createGunzip(), destination );
 };
 
 export async function getFileMeta( fileName: string ): Promise<FileMeta> {

commit 4a498c7e48ab375c391ed7ec43d15cd35764ee8e

Fix the PassThrough class import
- bug introduced in 711f20e6c34b10706f0a7677be030599056dcf61

- https://nodejs.org/api/stream.html#class-streampassthrough

src/lib/client-file-uploader.js
@@ -13,7 +13,8 @@ import fetch from 'node-fetch';
 import chalk from 'chalk';
 import { createGunzip, createGzip } from 'zlib';
 import { createHash } from 'crypto';
-import { pipeline, PassThrough } from 'node:stream/promises';
+import { pipeline } from 'node:stream/promises';
+import { PassThrough } from 'stream';
 import { Parser as XmlParser } from 'xml2js';
 import debugLib from 'debug';
 