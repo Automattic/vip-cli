7d4ecedef49849db2c96416a21eb027b14988177
Ahmed S. Wasif <ahmed.wasif@automattic.com>
2023-05-24 09:04:58 +0600

    Improve wording

--- src/bin/vip-export.js
+++ src/bin/vip-export.js
@@ -11,8 +11,8 @@ import command from '../lib/cli/command';
 import { trackEvent } from '../lib/tracker';
 
 command()
-	.command( 'sql', 'Import SQL to your database from a file' )
-	.example( 'vip export sql @mysite.develop', 'Export SQL file from your site' )
+	.command( 'sql', 'Export the contents of your database to an SQL file' )
+	.example( 'vip export sql @mysite.develop', 'Export the contents of your database to an SQL file' )
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_export_command_execute' );
 	} );
