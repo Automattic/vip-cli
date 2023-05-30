a0a255daf4a05f4007265d8d82564e0ee4a064bc
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-05-30 07:54:32 +0300

    Remove support for browser keychain

--- src/lib/keychain.ts
+++ src/lib/keychain.ts
@@ -7,27 +7,20 @@ import debugLib from 'debug';
  * Internal dependencies
  */
 import Insecure from './keychain/insecure';
-import Browser from './keychain/browser';
 import type { Keychain } from './keychain/keychain';
 
 let exportValue: Keychain;
-if ( typeof window === 'undefined' || typeof window.localStorage === 'undefined' ) {
-	// node
+const debug = debugLib( '@automattic/vip:keychain' );
 
-	const debug = debugLib( '@automattic/vip:keychain' );
+try {
+	// Try using Secure keychain ("keytar") first
+	const Secure = require( './keychain/secure' ) as Keychain & ( new() => Keychain );
+	exportValue = new Secure();
+} catch ( error ) {
+	debug( 'Cannot use Secure keychain; falling back to Insecure keychain (Details: %o)', error );
 
-	try {
-		// Try using Secure keychain ("keytar") first
-		const Secure = require( './keychain/secure' ) as Keychain & (new() => Keychain);
-		exportValue = new Secure();
-	} catch ( error ) {
-		debug( 'Cannot use Secure keychain; falling back to Insecure keychain (Details: %o)', error );
-
-		// Fallback to Insecure keychain if we can't
-		exportValue = new Insecure( 'vip-go-cli' );
-	}
-} else {
-	exportValue = new Browser();
+	// Fallback to Insecure keychain if we can't
+	exportValue = new Insecure( 'vip-go-cli' );
 }
 
 export default exportValue;
