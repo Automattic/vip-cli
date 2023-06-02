e9d106340b30e498f20a621b440fed7be095c30d
Pavel Schoffer <xschof00@gmail.com>
2023-06-02 10:31:44 +0200

    examples processing

--- helpers/generate-docs.js
+++ helpers/generate-docs.js
@@ -1,24 +1,24 @@
-const { spawn } = require('child_process');
-const path = require('path');
-const { once } = require('events');
+const { spawn } = require( 'child_process' );
+const path = require( 'path' );
+const { once } = require( 'events' );
 
-async function runCommand(subcommands) {
-	const childProcess = spawn('vip', subcommands);
+async function runCommand( subcommands ) {
+	const childProcess = spawn( 'vip', subcommands );
 
 	let output = '';
 
-	for await (const data of childProcess.stdout) {
+	for await ( const data of childProcess.stdout ) {
 		output += data.toString();
 	}
-	for await (const data of childProcess.stderr) {
+	for await ( const data of childProcess.stderr ) {
 		output += data.toString();
 	}
 
-	const [exitCode] = await once(childProcess, 'exit');
+	const [ exitCode ] = await once( childProcess, 'exit' );
 
-	if (exitCode !== 0) {
-		console.log('o', output);
-		throw new Error(`Script exited with code ${exitCode}`);
+	if ( exitCode !== 0 ) {
+		console.log( 'o', output );
+		throw new Error( `Script exited with code ${ exitCode }` );
 	}
 
 	return output.trim();
@@ -32,97 +32,114 @@ const SECTION_COMMAND = 'commands';
 const SECTION_OPTIONS = 'options';
 const SECTION_EXAMPLES = 'examples';
 
-const parseOutput = (output) => {
+const parseOutput = output => {
 	const result = {};
 
-	const lines = output.split('\n');
+	const lines = output.split( '\n' );
 	let currentSection = '';
 
-	for (let lineIx = 0; lineIx < lines.length; lineIx++) {
-		const line = lines[lineIx].trim();
-		if (!line) {
+	for ( let lineIx = 0; lineIx < lines.length; lineIx++ ) {
+		const line = lines[ lineIx ].trim();
+		if ( ! line ) {
 			continue;
 		}
-		if (line.startsWith('Usage:')) {
-			result.usage = line.match(USAGE_REGEXP)[1];
+		if ( line.startsWith( 'Usage:' ) ) {
+			result.usage = line.match( USAGE_REGEXP )[ 1 ];
 			continue;
 		}
 
-		if (line.startsWith('Commands:')) {
+		if ( line.startsWith( 'Commands:' ) ) {
 			result.commands = [];
 			currentSection = SECTION_COMMAND;
 			continue;
 		}
-		if (line.startsWith('Options:')) {
+		if ( line.startsWith( 'Options:' ) ) {
 			result.options = [];
 			currentSection = SECTION_OPTIONS;
 			continue;
 		}
-		if (line.startsWith('Examples:')) {
+		if ( line.startsWith( 'Examples:' ) ) {
 			result.examples = [];
+			result.examplesRaw = '';
 			currentSection = SECTION_EXAMPLES;
 			continue;
 		}
 
-		if (currentSection === SECTION_COMMAND) {
-			const [, command, description] = line.match(COMMAND_REGEXP);
-			result.commands.push({
+		if ( currentSection === SECTION_COMMAND ) {
+			const [ , command, description ] = line.match( COMMAND_REGEXP );
+			result.commands.push( {
 				command,
 				description,
-			});
+			} );
 			continue;
 		}
-		if (currentSection === SECTION_OPTIONS) {
-			if (line.match(OPTION_REGEXP)) {
-				const [, option, description] = line.match(OPTION_REGEXP);
-				result.options.push({
+		if ( currentSection === SECTION_OPTIONS ) {
+			if ( line.match( OPTION_REGEXP ) ) {
+				const [ , option, description ] = line.match( OPTION_REGEXP );
+				result.options.push( {
 					option,
 					description,
-				});
+				} );
 			} else {
-				console.log('Unknown option', line);
+				console.log( 'Unknown option', line );
 			}
 			continue;
 		}
-		if (currentSection === SECTION_EXAMPLES) {
+		if ( currentSection === SECTION_EXAMPLES ) {
 			let description = '';
-			while (!lines[lineIx].trim().startsWith('$')) {
-				description += lines[lineIx++];
+			while ( ! lines[ lineIx ].trim().startsWith( '$' ) ) {
+				const descriptionLine = lines[ lineIx ].trim();
+				if ( description ) {
+					description += '\n';
+				}
+				if ( result.examplesRaw ) {
+					result.examplesRaw += '\n';
+				}
+				result.examplesRaw += descriptionLine;
+				description += descriptionLine;
+				lineIx++;
 			}
-			const usage = lines[lineIx] && lines[lineIx].trim();
-			result.examples.push({
+			const usage = lines[ lineIx ] && lines[ lineIx ].trim();
+			result.examplesRaw += '\n' + usage;
+			result.examples.push( {
 				description,
 				usage,
-			});
+			} );
 		}
 	}
 
 	return result;
-}
+};
 
-const processCommand = async (subcommands) => {
-	console.error('Processing', subcommands.join(' '), '...');
+const processCommand = async subcommands => {
+	console.error( 'Processing', subcommands.join( ' ' ), '...' );
 
-	const output = await runCommand(subcommands.concat(['--help']));
-	const parsedOutput = parseOutput(output);
+	const output = await runCommand( subcommands.concat( [ '--help' ] ) );
+	const parsedOutput = parseOutput( output );
 
 	const commandCount = parsedOutput.commands?.length || 0;
-	for (let commandIx = 0; commandIx < commandCount; commandIx++) {
-		const element = parsedOutput.commands[commandIx];
-		const commandOutput = await processCommand(subcommands.concat([element.command]));
+	const commandPromises = [];
+	for ( let commandIx = 0; commandIx < commandCount; commandIx++ ) {
+		const element = parsedOutput.commands[ commandIx ];
+		commandPromises.push( processCommand( subcommands.concat( [ element.command ] ) ) );
+	}
+	const commandOutputs = await Promise.all( commandPromises );
+	for ( let commandIx = 0; commandIx < commandCount; commandIx++ ) {
+		const element = parsedOutput.commands[ commandIx ];
+		const commandOutput = commandOutputs[ commandIx ];
 		commandOutput.name = element.command;
 		commandOutput.description = element.description;
-		parsedOutput.commands[commandIx] = commandOutput;
+		parsedOutput.commands[ commandIx ] = commandOutput;
 	}
 
 	return parsedOutput;
-}
+};
 
-(async () => {
-	const version = await runCommand(['--version'])
+( async () => {
+	const version = await runCommand( [ '--version' ] );
 
-	const result = await processCommand([]);
+	const result = await processCommand( [ ] );
 	result.version = version;
 
-	console.log(JSON.stringify(result, null, 2));
-})()
+	console.log( JSON.stringify( result, null, 2 ) );
+} )();
