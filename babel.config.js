b457b090864172398cceec95d36c47409eec5c4e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-06-05 14:31:01 +0300

    Revert "Add support for partial mocking for ES-native module exports"
    This reverts commit 10fc5f368f6909586ed64f728ba9bfd04888aa02.

--- babel.config.js
+++ babel.config.js
@@ -30,13 +30,6 @@ module.exports = {
 			},
 		],
 	],
-	env: {
-		test: {
-			// see https://github.com/facebook/jest/issues/936#issuecomment-821944391
-			// Jest has no good way of partially mocking a module
-			plugins: ['explicit-exports-references']
-		}
-	},
 	ignore: [
 		"**/*.d.ts"
 	]
