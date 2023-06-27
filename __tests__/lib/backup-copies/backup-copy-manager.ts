4320339ffc2c3e4e0dadeabb38858c4c9d0b6228
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-06-27 12:14:24 +0800

    WIP

---
+++ __tests__/lib/backup-copies/backup-copy-manager.ts
@@ -0,0 +1,11 @@
+import { describe, it } from '@jest/globals';
+import { ApolloProvider, gql, InMemoryCache } from '@apollo/client';
+import { MockedProvider } from '@apollo/client/testing';
+
+describe( 'backup-copy-manager', () => {
+	describe( 'getRemoteBackups', () => {
+		it( 'should get remote backups', () => {
+
+		} );
+	} );
+} );
