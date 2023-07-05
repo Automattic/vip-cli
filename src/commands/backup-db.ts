97c55fe24d44cdbff22b02ef4e46e18cfec6df47
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-07-05 10:19:45 +0800

    Rewrite new files as TypeScript files

--- src/commands/backup-db.ts
+++ src/commands/backup-db.ts
@@ -20,6 +20,7 @@ import API, {
 import * as exit from '../lib/cli/exit';
 import { pollUntil } from '../lib/utils';
 import { ProgressTracker } from '../lib/cli/progress';
+import { CommandTracker } from '../lib/tracker';
 
 const DB_BACKUP_PROGRESS_POLL_INTERVAL = 1000;
 
@@ -30,6 +31,23 @@ export const CREATE_DB_BACKUP_JOB_MUTATION = gql`
 		}
 	}
 `;
+
+// TODO: Replace this with the codegen
+interface Job {
+	inProgressLock: string;
+	metadata: { name: string; value: string }[];
+	progress?: { status: string };
+	completedAt: string;
+}
+
+interface App {
+	id: number;
+}
+
+interface Environment {
+	id: number;
+	jobs: Job[];
+}
 
 export const DB_BACKUP_JOB_STATUS_QUERY = gql`
 	query AppBackupJobStatus($appId: Int!, $envId: Int!) {
@@ -56,10 +74,14 @@ export const DB_BACKUP_JOB_STATUS_QUERY = gql`
 	}
 `;
 
-async function getBackupJob( appId, envId ) {
+async function getBackupJob( appId: number, envId: number ) {
 	const api = await API();
 
-	const response = await api.query( {
+	const response = await api.query< {
+		app: {
+			environments: Environment[];
+		};
+	} >( {
 		query: DB_BACKUP_JOB_STATUS_QUERY,
 		variables: { appId, envId },
 		fetchPolicy: 'network-only',
@@ -76,7 +98,7 @@ async function getBackupJob( appId, envId ) {
 	return job || null;
 }
 
-async function createBackupJob( appId, envId ) {
+async function createBackupJob( appId: number, envId: number ) {
 	// Disable global error handling so that we can handle errors ourselves
 	disableGlobalGraphQLErrorHandling();
 
@@ -97,36 +119,39 @@ async function createBackupJob( appId, envId ) {
 
 // Library for a possible command in the future: vip backup db @app.env
 export class BackupDBCommand {
-	app;
-	env;
-	job;
-	jobStatus;
-	jobAge;
-	backupName;
-	silent;
+	app: App;
+	env: Environment;
+	job?: Job;
+	jobStatus?: string;
+	jobAge?: number;
+	backupName?: string;
+	silent?: boolean;
 	steps = {
 		PREPARE: 'prepare',
 		GENERATE: 'generate',
 	};
-	track;
+	track: CommandTracker;
+	private progressTracker: ProgressTracker;
 
-	constructor( app, env, trackerFn = () => {} ) {
+	constructor( app: App, env: Environment, trackerFn = async () => {} ) {
 		this.app = app;
 		this.env = env;
 		this.progressTracker = new ProgressTracker( [
 			{ id: this.steps.PREPARE, name: 'Preparing for backup generation' },
 			{ id: this.steps.GENERATE, name: 'Generating backup' },
 		] );
-		this.track = trackerFn;
+		this.track = trackerFn as CommandTracker;
 	}
 
-	log( msg ) {
-		if ( this.silent ) return;
+	log( msg: string ) {
+		if ( this.silent ) {
+			return;
+		}
 		console.log( msg );
 	}
 
-	isDone( job ) {
-		return ! job.inProgressLock;
+	isDone( job?: Job ) {
+		return ! job?.inProgressLock;
 	}
 
 	/**
@@ -146,7 +171,8 @@ export class BackupDBCommand {
 		this.jobStatus = this.job?.progress?.status;
 
 		if ( this.job?.completedAt ) {
-			this.jobAge = ( new Date() - new Date( this.job.completedAt ) ) / 1000 / 60;
+			this.jobAge =
+				( new Date().getTime() - new Date( this.job.completedAt ).getTime() ) / 1000 / 60;
 		} else {
 			this.jobAge = undefined;
 		}
@@ -174,7 +200,8 @@ export class BackupDBCommand {
 				this.progressTracker.stepRunning( this.steps.PREPARE );
 				this.progressTracker.startPrinting();
 				await createBackupJob( this.app.id, this.env.id );
-			} catch ( err ) {
+			} catch ( e ) {
+				const err = e as Error;
 				this.progressTracker.stepFailed( this.steps.PREPARE );
 				this.stopProgressTracker();
 				if ( err.message?.includes( 'Database backups limit reached' ) ) {
@@ -217,7 +244,8 @@ export class BackupDBCommand {
 				DB_BACKUP_PROGRESS_POLL_INTERVAL,
 				this.isDone.bind( this )
 			);
-		} catch ( err ) {
+		} catch ( e ) {
+			const err = e as Error;
 			this.progressTracker.stepFailed( this.steps.GENERATE );
 			this.stopProgressTracker();
 			await this.track( 'error', {
