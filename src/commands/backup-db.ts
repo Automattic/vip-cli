d4069313590a5a79c0594e3e21dcaffdcd1a8067
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-07-12 15:46:47 +0800

    Fix eslint warnings

--- src/commands/backup-db.ts
+++ src/commands/backup-db.ts
@@ -23,8 +23,12 @@ import { ProgressTracker } from '../lib/cli/progress';
 import { CommandTracker } from '../lib/tracker';
 import { GraphQLFormattedError } from 'graphql';
 import { RateLimitExceededError } from '../lib/types/graphql/rate-limit-exceeded-error';
+import { AppBackupJobStatusQuery } from './backup-db.generated';
+import { App, AppEnvironment, Job } from '../graphqlTypes';
 
 const DB_BACKUP_PROGRESS_POLL_INTERVAL = 1000;
+
+// type Job = AppBackupJobStatusQuery['app']
 
 export const CREATE_DB_BACKUP_JOB_MUTATION = gql`
 	mutation TriggerDatabaseBackup($input: AppEnvironmentTriggerDBBackupInput) {
@@ -33,23 +37,6 @@ export const CREATE_DB_BACKUP_JOB_MUTATION = gql`
 		}
 	}
 `;
-
-// TODO: Replace this with the codegen
-interface Job {
-	inProgressLock: boolean;
-	metadata: { name: string; value: string }[];
-	progress?: { status: string };
-	completedAt: string;
-}
-
-interface App {
-	id: number;
-}
-
-interface Environment {
-	id: number;
-	jobs: Job[];
-}
 
 export const DB_BACKUP_JOB_STATUS_QUERY = gql`
 	query AppBackupJobStatus($appId: Int!, $envId: Int!) {
@@ -79,25 +66,17 @@ export const DB_BACKUP_JOB_STATUS_QUERY = gql`
 async function getBackupJob( appId: number, envId: number ) {
 	const api = await API();
 
-	const response = await api.query< {
-		app: {
-			environments: Environment[];
-		};
-	} >( {
+	const response = await api.query< AppBackupJobStatusQuery >( {
 		query: DB_BACKUP_JOB_STATUS_QUERY,
 		variables: { appId, envId },
 		fetchPolicy: 'network-only',
 	} );
 
 	const {
-		data: {
-			app: { environments },
-		},
+		data: { app },
 	} = response;
 
-	const job = environments[ 0 ].jobs[ 0 ];
-
-	return job || null;
+	return app?.environments?.[ 0 ]?.jobs?.[ 0 ] as Job | undefined;
 }
 
 async function createBackupJob( appId: number, envId: number ) {
@@ -122,7 +101,7 @@ async function createBackupJob( appId: number, envId: number ) {
 // Library for a possible command in the future: vip backup db @app.env
 export class BackupDBCommand {
 	app: App;
-	env: Environment;
+	env: AppEnvironment;
 	job?: Job;
 	jobStatus?: string;
 	jobAge?: number;
@@ -135,7 +114,7 @@ export class BackupDBCommand {
 	track: CommandTracker;
 	private progressTracker: ProgressTracker;
 
-	constructor( app: App, env: Environment, trackerFn = async () => {} ) {
+	constructor( app: App, env: AppEnvironment, trackerFn = async () => {} ) {
 		this.app = app;
 		this.env = env;
 		this.progressTracker = new ProgressTracker( [
@@ -167,10 +146,10 @@ export class BackupDBCommand {
 	}
 
 	async loadBackupJob() {
-		this.job = await getBackupJob( this.app.id, this.env.id );
+		this.job = await getBackupJob( this.app.id ?? 0, this.env.id ?? 0 );
 		this.backupName =
-			this.job?.metadata.find( meta => meta.name === 'backupName' )?.value || 'Unknown';
-		this.jobStatus = this.job?.progress?.status;
+			this.job?.metadata?.find( meta => meta?.name === 'backupName' )?.value ?? 'Unknown';
+		this.jobStatus = this.job?.progress?.status ?? undefined;
 
 		if ( this.job?.completedAt ) {
 			this.jobAge =
@@ -201,20 +180,20 @@ export class BackupDBCommand {
 				this.log( 'Generating a new database backup...' );
 				this.progressTracker.stepRunning( this.steps.PREPARE );
 				this.progressTracker.startPrinting();
-				await createBackupJob( this.app.id, this.env.id );
-			} catch ( e ) {
-				const err = e as Error & {
+				await createBackupJob( this.app.id ?? 0, this.env.id ?? 0 );
+			} catch ( err ) {
+				const error = err as Error & {
 					graphQLErrors?: GraphQLFormattedError[];
 				};
-				const graphQLError = err.graphQLErrors?.[ 0 ] as RateLimitExceededError | undefined;
+				const graphQLError = error.graphQLErrors?.[ 0 ] as RateLimitExceededError | undefined;
 				this.progressTracker.stepFailed( this.steps.PREPARE );
 				this.stopProgressTracker();
 				if ( graphQLError?.extensions?.errorHttpCode === 429 ) {
 					const retryAfter = graphQLError.extensions.retryAfter;
 					await this.track( 'error', {
 						error_type: 'rate_limit_exceeded',
-						error_message: `Couldn't create a new database backup job: ${ err?.message }`,
-						stack: err?.stack,
+						error_message: `Couldn't create a new database backup job: ${ error.message }`,
+						stack: error.stack,
 					} );
 					const errMessage = `A new database backup was not generated because a recently generated backup already exists.
 If you would like to run the same command, you can retry on or after: ${ retryAfter }
@@ -227,10 +206,10 @@ Learn more about limitations around generating database backups: https://docs.wp
 				}
 				await this.track( 'error', {
 					error_type: 'db_backup_job_creation_failed',
-					error_message: `Database Backup job creation failed: ${ err?.message }`,
-					stack: err?.stack,
+					error_message: `Database Backup job creation failed: ${ error.message }`,
+					stack: error.stack,
 				} );
-				exit.withError( `Couldn't create a new database backup job: ${ err?.message }` );
+				exit.withError( `Couldn't create a new database backup job: ${ error.message }` );
 			}
 		}
 
@@ -244,16 +223,16 @@ Learn more about limitations around generating database backups: https://docs.wp
 				DB_BACKUP_PROGRESS_POLL_INTERVAL,
 				this.isDone.bind( this )
 			);
-		} catch ( e ) {
-			const err = e as Error;
+		} catch ( err ) {
+			const error = err as Error;
 			this.progressTracker.stepFailed( this.steps.GENERATE );
 			this.stopProgressTracker();
 			await this.track( 'error', {
 				error_type: 'db_backup_job_failed',
-				error_message: `Database Backup job failed: ${ err?.message }`,
-				stack: err?.stack,
+				error_message: `Database Backup job failed: ${ error.message }`,
+				stack: error.stack,
 			} );
-			exit.withError( `Failed to create new database backup: ${ err?.message }` );
+			exit.withError( `Failed to create new database backup: ${ error.message }` );
 		}
 
 		this.progressTracker.stepSuccess( this.steps.GENERATE );
