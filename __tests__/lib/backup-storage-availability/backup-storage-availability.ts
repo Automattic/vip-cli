5afbd830b0f20fdbb6f481ea8b68758bf027e434
Abdullah bin Kasim <abdullah.kasim@automattic.com>
2023-07-31 15:04:32 +0800

    Update tests to reflect number of prompt changes

--- __tests__/lib/backup-storage-availability/backup-storage-availability.ts
+++ __tests__/lib/backup-storage-availability/backup-storage-availability.ts
@@ -53,13 +53,14 @@ describe( 'backup-storage-availability', () => {
 
 	describe( 'validateAndPromptDiskSpaceWarningForDevEnvBackupImport', () => {
 		it.each( [
-			{ dockerSpace: 10, vipSpace: 1 },
-			{ dockerSpace: 1, vipSpace: 10 },
-			{ dockerSpace: 4, vipSpace: 10 },
-			{ dockerSpace: 10, vipSpace: 5 },
+			{ dockerSpace: 10, vipSpace: 1, timesPrompted: 1 },
+			{ dockerSpace: 1, vipSpace: 10, timesPrompted: 1 },
+			{ dockerSpace: 4, vipSpace: 10, timesPrompted: 1 },
+			{ dockerSpace: 10, vipSpace: 5, timesPrompted: 1 },
+			{ dockerSpace: 0.5, vipSpace: 0.5, timesPrompted: 2 },
 		] )(
 			"should show a prompt if there's not enough space available - dockerSpace: $dockerSpace GiB, vipSpace: $vipSpace GiB",
-			async ( { dockerSpace, vipSpace } ) => {
+			async ( { dockerSpace, vipSpace, timesPrompted } ) => {
 				// backup size is 1 GiB
 				const backupStorageAvailability = new BackupStorageAvailability( oneGiBInBytes );
 				jest
@@ -73,7 +74,48 @@ describe( 'backup-storage-availability', () => {
 					backupStorageAvailability.validateAndPromptDiskSpaceWarningForDevEnvBackupImport()
 				).resolves.toBe( true );
 
-				expect( confirmRunSpy ).toHaveBeenCalled();
+				expect( confirmRunSpy ).toHaveBeenCalledTimes( timesPrompted );
+			}
+		);
+
+		it( 'should exit early if we press no on the first prompt', async () => {
+			confirmRunSpy.mockResolvedValueOnce( false );
+			// backup size is 1 GiB
+			const backupStorageAvailability = new BackupStorageAvailability( oneGiBInBytes );
+			jest
+				.spyOn( backupStorageAvailability, 'getStorageAvailableInVipPath' )
+				.mockResolvedValue( Math.round( 0.5 * oneGiBInBytes ) );
+			jest
+				.spyOn( backupStorageAvailability, 'getDockerStorageAvailable' )
+				.mockReturnValue( Math.round( 0.5 * oneGiBInBytes ) );
+
+			await expect(
+				backupStorageAvailability.validateAndPromptDiskSpaceWarningForDevEnvBackupImport()
+			).resolves.toBe( false );
+
+			expect( confirmRunSpy ).toHaveBeenCalledTimes( 1 );
+		} );
+
+		it.each( [
+			{ kiBRaw: '', vipSpace: 10, timesPrompted: 0 },
+			{ kiBRaw: undefined, vipSpace: 10, timesPrompted: 0 },
+			{ kiBRaw: '', vipSpace: 1, timesPrompted: 1 },
+			{ kiBRaw: undefined, vipSpace: 1, timesPrompted: 1 },
+		] )(
+			'should not validate Docker Machine storage if the Docker Machine is not available - kiBRaw: $kiBRaw GiB, vipSpace: $vipSpace GiB',
+			async ( { kiBRaw, vipSpace, timesPrompted } ) => {
+				// backup size is 1 GiB
+				const backupStorageAvailability = new BackupStorageAvailability( oneGiBInBytes );
+				jest
+					.spyOn( backupStorageAvailability, 'getStorageAvailableInVipPath' )
+					.mockResolvedValue( Math.round( vipSpace * oneGiBInBytes ) );
+				jest.spyOn( backupStorageAvailability, 'getDockerStorageKiBRaw' ).mockReturnValue( kiBRaw );
+
+				await expect(
+					backupStorageAvailability.validateAndPromptDiskSpaceWarningForDevEnvBackupImport()
+				).resolves.toBe( true );
+
+				expect( confirmRunSpy ).toHaveBeenCalledTimes( timesPrompted );
 			}
 		);
 
