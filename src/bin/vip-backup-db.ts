99991c82f20b4b0193d751323094f350381d2008
Ahmed Sayeed Wasif <aswasif007@gmail.com>
2023-08-24 13:48:13 +0600

    feat(backup): Add vip backup db command (#1484)
    * Add vip backup db command
    
    * Update file permission
    
    * Move export specific command to suitable file
    
    * Format duration to human readable string
    
    * Add tests for formatDuration
    
    * Fix example
    
    * Address minor PR feedback
    
    * Add test case for from > to
    
    * Check if duration left is <1000ms instead of <0ms
    
    Co-authored-by: abdullah-kasim <abdullah.kasim@automattic.com>
    
    ---------
    
    Co-authored-by: abdullah-kasim <abdullah.kasim@automattic.com>

---
+++ src/bin/vip-backup-db.ts
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+/**
+ * @flow
+ * @format
+ */
+
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import command from '../lib/cli/command';
+
+import { makeCommandTracker } from '../lib/tracker';
+import { App, AppEnvironment, Job } from '../graphqlTypes';
+import { BackupDBCommand } from '../commands/backup-db';
+
+const examples = [
+	{
+		usage: 'vip backup db @mysite.develop',
+		description: 'Trigger a new backup for your database of the @mysite.develop environment',
+	},
+];
+
+const appQuery = `
+	id,
+	name,
+	type,
+	organization { id, name },
+	environments{
+		id
+		appId
+		type
+		name
+		primaryDomain { name }
+		uniqueLabel
+	}
+`;
+
+void command( {
+	appContext: true,
+	appQuery,
+	envContext: true,
+	module: 'backup-db',
+	requiredArgs: 0,
+	usage: 'vip backup db',
+} )
+	.examples( examples )
+	.argv( process.argv, async ( arg: string[], { app, env }: { app: App; env: AppEnvironment } ) => {
+		const trackerFn = makeCommandTracker( 'backup_db', {
+			app: app.id,
+			env: env.uniqueLabel,
+		} );
+		await trackerFn( 'execute' );
+
+		const cmd = new BackupDBCommand( app, env, trackerFn );
+		await cmd.run();
+
+		await trackerFn( 'success' );
+	} );
---
+++ src/bin/vip-backup.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+import command from '../lib/cli/command';
+import { trackEvent } from '../lib/tracker';
+
+void command( { usage: 'vip backup' } )
+	.command( 'db', 'Trigger a new backup for your database' )
+	.example(
+		'vip backup db @mysite.develop',
+		'Trigger a new backup for your database of the @mysite.develop environment'
+	)
+	.argv( process.argv, async () => {
+		await trackEvent( 'vip_backup_command_execute' );
+	} );
