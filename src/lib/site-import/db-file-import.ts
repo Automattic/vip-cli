355ff2deb012896793307c3daf4421b66cb0a2f8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-08-28 13:11:00 +0300

    Refactor type definitions

--- src/lib/site-import/db-file-import.ts
+++ src/lib/site-import/db-file-import.ts
@@ -1,42 +1,23 @@
 /**
  * Internal dependencies
  */
-import {
-	App,
-	AppEnvironment,
-	AppEnvironmentSyncProgress,
-	Domain,
-	Maybe,
-	Organization,
-} from '../../graphqlTypes';
+import { App, AppEnvironment } from '../../graphqlTypes';
 import { GB_IN_BYTES } from '../../lib/constants/file-size';
 
 export const SQL_IMPORT_FILE_SIZE_LIMIT = 100 * GB_IN_BYTES;
 export const SQL_IMPORT_FILE_SIZE_LIMIT_LAUNCHED = 5 * GB_IN_BYTES;
 
-export interface AppForImport {
-	id: number;
-	environments: Maybe< Maybe< AppEnvironment >[] >;
-	name: string;
-	organization: Maybe< Organization >;
-	type: string;
-}
+export type AppForImport = Pick< App, 'id' | 'environments' | 'name' | 'organization' | 'type' >;
 
 export interface ImportStatusType {
 	dbOperationInProgress: boolean;
 	importInProgress: boolean;
 }
 
-export interface EnvForImport {
-	id: number;
-	appId: number;
-	name: string;
-	type: string;
-	primaryDomain: Domain;
-	syncProgress: AppEnvironmentSyncProgress;
-	importStatus: ImportStatusType;
-	launched: boolean;
-}
+export type EnvForImport = Pick<
+	AppEnvironment,
+	'id' | 'appId' | 'name' | 'type' | 'primaryDomain' | 'syncProgress' | 'importStatus' | 'launched'
+>;
 
 export function currentUserCanImportForApp( app: App | AppForImport ): boolean {
 	// TODO: implement
@@ -46,9 +27,9 @@ export function currentUserCanImportForApp( app: App | AppForImport ): boolean {
 export const SUPPORTED_DB_FILE_IMPORT_SITE_TYPES = [ 'WordPress' ];
 
 export const isSupportedApp = ( { type }: AppForImport ) =>
-	SUPPORTED_DB_FILE_IMPORT_SITE_TYPES.includes( type );
+	SUPPORTED_DB_FILE_IMPORT_SITE_TYPES.includes( type! );
 
 export const SYNC_STATUS_NOT_SYNCING = 'not_syncing';
 
-export const isImportingBlockedBySync = ( { syncProgress: { status } }: EnvForImport ) =>
-	status !== SYNC_STATUS_NOT_SYNCING;
+export const isImportingBlockedBySync = ( env: EnvForImport ) =>
+	env.syncProgress?.status !== SYNC_STATUS_NOT_SYNCING;
