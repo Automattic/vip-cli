1d5ea625d806154ba53ba132dee398f28be45756
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-10-20 14:55:19 +0300

    Address code review comments

--- src/lib/constants/dev-environment.ts
+++ src/lib/constants/dev-environment.ts
@@ -34,11 +34,11 @@ interface PhpImage {
 }
 
 export const DEV_ENVIRONMENT_PHP_VERSIONS: Record< string, PhpImage > = {
-	8.1: { image: 'ghcr.io/automattic/vip-container-images/php-fpm:8.1', label: '8.1 (recommended)' },
 	8.2: {
 		image: 'ghcr.io/automattic/vip-container-images/php-fpm:8.2',
-		label: '8.2 (experimental)',
+		label: '8.2 (recommended)',
 	},
+	8.1: { image: 'ghcr.io/automattic/vip-container-images/php-fpm:8.1', label: '8.1' },
 	8.3: {
 		image: 'ghcr.io/automattic/vip-container-images/php-fpm:8.3',
 		label: '8.3 (experimental, not supported)',
