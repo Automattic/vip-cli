f0518cc900767a82545733627ee327dbc70d6c58
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-12-01 11:39:32 +0200

    fix: bump local environment version to 2.0.1

--- src/lib/constants/dev-environment.ts
+++ src/lib/constants/dev-environment.ts
@@ -53,4 +53,4 @@ export const DEV_ENVIRONMENT_PHP_VERSIONS: Record< string, PhpImage > = {
 	},
 } as const;
 
-export const DEV_ENVIRONMENT_VERSION = '2.0.0';
+export const DEV_ENVIRONMENT_VERSION = '2.0.1';
