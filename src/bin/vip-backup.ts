4e45389ce1653571bc2c8db5a331a13984c6f766
Yoli Hodde <yoli.hodde@automattic.com>
2024-04-09 16:33:58 -0700

    Updates to descriptions and examples for vip backup

--- src/bin/vip-backup.ts
+++ src/bin/vip-backup.ts
@@ -4,10 +4,14 @@ import command from '../lib/cli/command';
 import { trackEvent } from '../lib/tracker';
 
 void command( { usage: 'vip backup' } )
-	.command( 'db', 'Trigger a new backup for your database' )
+	.command( 'db', 'Generate a fresh database backup for an environment.' )
 	.example(
-		'vip backup db @mysite.develop',
-		'Trigger a new backup for your database of the @mysite.develop environment'
+		'vip @example-app.develop backup db\n' +
+			'      Generating a new database backup...\n' +
+			'      ✓ Preparing for backup generation\n' +
+			'      ✓ Generating backup\n' +
+			'      New database backup created',
+		'Generate a fresh database backup for an environment.'
 	)
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_backup_command_execute' );
