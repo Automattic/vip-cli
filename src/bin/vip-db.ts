f7b69af723d09320bb9291dcc4107de7a6af17bf
Yoli Hodde <yoli.hodde@automattic.com>
2024-04-09 15:59:56 -0700

    prettier linting

--- src/bin/vip-db.ts
+++ src/bin/vip-db.ts
@@ -14,7 +14,7 @@ void command( { usage: 'vip db' } )
 	.command( 'phpmyadmin', 'Access a read-only phpMyAdmin console for an environment database.' )
 	.example(
 		'vip @example-app.develop db phpmyadmin',
-		'Access a read-only phpMyAdmin console for the environment\'s database.'
+		"Access a read-only phpMyAdmin console for the environment's database."
 	)
 	.argv( process.argv, async () => {
 		await trackEvent( 'vip_db_command_execute' );
