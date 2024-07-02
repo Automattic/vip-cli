d8d41ea335d73796de6203535a10b8064d793169
Ariana Kataoka <ariskataoka@users.noreply.github.com>
2024-07-02 21:16:04 +1000

    Fix tests

--- __tests__/lib/vip-import-validate-files.js
+++ __tests__/lib/vip-import-validate-files.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
+
 import {
 	folderStructureValidation,
 	isFileSanitized,
+	validateFiles,
 } from '../../src/lib/vip-import-validate-files';
 
 global.console = { log: jest.fn() };
@@ -44,6 +47,73 @@ describe( 'lib/vip-import-validate-files', () => {
 			const isSanitized = isFileSanitized( image );
 
 			expect( isSanitized ).toBe( true );
+		} );
+	} );
+
+	describe( 'validateFiles()', () => {
+		const mediaImportConfig = {
+			allowedFileTypes: { jpg: 'image/jpeg', png: 'image/png' },
+			fileSizeLimitInBytes: 5000000, // 5MB
+			fileNameCharCount: 255,
+		};
+
+		afterEach( () => {
+			jest.clearAllMocks();
+		} );
+
+		it( 'should detect invalid file types', async () => {
+			jest
+				.spyOn( fs.promises, 'stat' )
+				.mockResolvedValue( { isDirectory: () => false, size: 4000 } );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 10 } );
+
+			const result = await validateFiles( [ 'file1.txt', 'file2.exe' ], mediaImportConfig );
+			expect( result.errorFileTypes ).toEqual( [ 'file1.txt', 'file2.exe' ] );
+		} );
+
+		it( 'should detect valid file types and invalid file sizes', async () => {
+			jest.spyOn( fs.promises, 'stat' ).mockResolvedValue( { isDirectory: () => false } );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 6000000 } );
+
+			const result = await validateFiles( [ 'file1.jpg', 'file2.png' ], mediaImportConfig );
+			expect( result.errorFileSizes ).toEqual( [ 'file1.jpg', 'file2.png' ] );
+		} );
+
+		it( 'should detect valid file types and valid file sizes', async () => {
+			jest.spyOn( fs.promises, 'stat' ).mockResolvedValue( { isDirectory: () => false } );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 4000 } );
+
+			const result = await validateFiles( [ 'file1.jpg', 'file2.png' ], mediaImportConfig );
+			expect( result.errorFileTypes ).toEqual( [] );
+			expect( result.errorFileSizes ).toEqual( [] );
+		} );
+
+		it( 'should detect files with invalid filenames', async () => {
+			jest.spyOn( fs.promises, 'stat' ).mockResolvedValue( { isDirectory: () => false } );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 4000 } );
+			const result = await validateFiles(
+				[ 'file%20name.jpg', 'file+name.png' ],
+				mediaImportConfig
+			);
+			expect( result.errorFileNames ).toEqual( [ 'file%20name.jpg', 'file+name.png' ] );
+		} );
+
+		it( 'should detect files with filenames exceeding character count limit', async () => {
+			jest.spyOn( fs.promises, 'stat' ).mockResolvedValue( { isDirectory: () => false } );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 4000 } );
+
+			const longFileName = 'a'.repeat( 256 ) + '.jpg';
+			const result = await validateFiles( [ longFileName ], mediaImportConfig );
+			expect( result.errorFileNamesCharCount ).toEqual( [ longFileName ] );
+		} );
+
+		it( 'should detect intermediate images', async () => {
+			jest.spyOn( fs.promises, 'stat' ).mockResolvedValue( { isDirectory: () => false } );
+			jest.spyOn( fs, 'existsSync' ).mockReturnValue( true );
+			jest.spyOn( fs, 'statSync' ).mockReturnValue( { size: 4000 } );
+
+			const result = await validateFiles( [ 'image-4000x6000.jpg' ], mediaImportConfig );
+			expect( result.intermediateImagesTotal ).toBe( 1 );
 		} );
 	} );
 } );
